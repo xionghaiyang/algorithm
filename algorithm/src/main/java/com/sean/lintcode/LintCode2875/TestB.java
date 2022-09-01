36f1b45c8826fdacee6093a13aa5f3c8393c0ed5
xionghaiyang <18756618469@163.com>
2022-09-01 12:02:39 +0800

    2875 · 程序运行结果

---
+++ algorithm/src/main/java/com/sean/lintcode/LintCode2875/TestB.java
@@ -0,0 +1,21 @@
+package com.sean.lintcode.LintCode2875;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-08-30 08:27
+ */
+public class TestB extends TestA {
+
+    static {
+        num++;
+    }
+
+    {
+        num++;
+    }
+
+    public TestB() {
+        num++;
+    }
+
+}
