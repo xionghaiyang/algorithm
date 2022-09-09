7a6f047aef2633fd38214b257ea5c6ff005dc181
xionghaiyang <18756618469@163.com>
2022-09-09 08:58:54 +0800

    35. 搜索插入位置

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode35/Solution.java
@@ -0,0 +1,29 @@
+package com.sean.leetcode.LeetCode35;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-09-08 20:57
+ * @Description: https://leetcode.cn/problems/search-insert-position/?plan=algorithms&plan_progress=zq05vcm
+ * 35. 搜索插入位置
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+ * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ */
+public class Solution {
+
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1, res = n;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (target <= nums[mid]) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+
+}
