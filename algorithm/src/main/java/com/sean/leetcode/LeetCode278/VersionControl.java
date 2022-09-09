e1d39fff3650e8e5619bad9aeadbd1faa451134b
xionghaiyang <18756618469@163.com>
2022-09-09 08:54:22 +0800

    278. 第一个错误的版本

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode278/VersionControl.java
@@ -0,0 +1,13 @@
+package com.sean.leetcode.LeetCode278;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-09-08 20:52
+ */
+public class VersionControl {
+
+    public boolean isBadVersion(int version) {
+        return true;
+    }
+
+}
