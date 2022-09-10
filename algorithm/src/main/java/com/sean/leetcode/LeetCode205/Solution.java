92bc9caaea0a19fde41f7c404f6809bf419be77c
xionghaiyang <18756618469@163.com>
2022-09-10 11:02:23 +0800

    205. 同构字符串

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode205/Solution.java
@@ -0,0 +1,51 @@
+package com.sean.leetcode.LeetCode205;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-09-09 21:01
+ * @Description: https://leetcode.cn/problems/isomorphic-strings/?plan=leetcode_75&plan_progress=zq8h8ym
+ * 205. 同构字符串
+ * 给定两个字符串 s 和 t ，判断它们是否是同构的。
+ * 如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
+ * 每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。
+ * 不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
+ */
+public class Solution {
+
+    public boolean isIsomorphic(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int n = s.length();
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (map.containsKey(c1)) {
+                if (c2 != map.get(c1)) {
+                    return false;
+                }
+            } else {
+                if (set.contains(c2)) {
+                    return false;
+                }
+                map.put(c1, c2);
+                set.add(c2);
+            }
+        }
+        return true;
+    }
+
+}
