525a552aee0c73f3fc88ccc6644693f596555c0e
xionghaiyang <18756618469@163.com>
2022-10-15 20:59:59 +0800

    剑指 Offer 22. 链表中倒数第k个节点

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeOffer22/Solution.java
@@ -0,0 +1,34 @@
+package com.sean.leetcode.LeetCodeOffer22;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-09-28 22:45
+ * @Description: https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/?plan=lcof&plan_progress=zq3t7ii
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ */
+public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode fast = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            head = head.next;
+            fast = fast.next;
+        }
+        return head;
+    }
+
+}
