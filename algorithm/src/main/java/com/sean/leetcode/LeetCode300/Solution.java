2945a9dd9710c39e4849d98f4ce4a6d5cd41b763
xionghaiyang <18756618469@163.com>
2023-01-04 10:44:20 +0800

    300. 最长递增子序列

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode300/Solution.java
@@ -0,0 +1,34 @@
+package com.sean.leetcode.LeetCode300;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-12-29 10:46
+ * @Description: https://leetcode.cn/problems/longest-increasing-subsequence/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * 300. 最长递增子序列
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
+ * 例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ */
+public class Solution {
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+}
