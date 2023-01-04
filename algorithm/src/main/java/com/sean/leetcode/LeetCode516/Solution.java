76be6a14bddca5f056737f16ec8119b50076d34d
xionghaiyang <18756618469@163.com>
2023-01-04 10:33:52 +0800

    516. 最长回文子序列

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode516/Solution.java
@@ -0,0 +1,33 @@
+package com.sean.leetcode.LeetCode516;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-12-29 10:33
+ * @Description: https://leetcode.cn/problems/longest-palindromic-subsequence/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * 516. 最长回文子序列
+ * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+ * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ */
+public class Solution {
+
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        //dp[i][j]表示s[i...j]范围最长的回文子序列长度
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+}
