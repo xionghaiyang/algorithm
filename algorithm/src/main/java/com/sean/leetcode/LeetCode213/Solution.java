765044535a40aa50b62ff3d6d8185e5d5c6b1543
xionghaiyang <18756618469@163.com>
2023-01-05 09:20:37 +0800

    213. 打家劫舍 II

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode213/Solution.java
@@ -0,0 +1,37 @@
+package com.sean.leetcode.LeetCode213;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-12-14 20:03
+ * @Description: https://leetcode.cn/problems/house-robber-ii/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * 213. 打家劫舍 II
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。
+ * 这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+ */
+public class Solution {
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        } else if (n == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(process(nums, 0, n - 2), process(nums, 1, n - 1));
+    }
+
+    private int process(int[] nums, int start, int end) {
+        int first = nums[start], second = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+}
