89d57b25e3aa8faae914a0c6d947b1ffa4e2dab8
xionghaiyang <18756618469@163.com>
2023-01-05 11:09:35 +0800

    将单向链表按某值划分成左边小、中间相等、右边大的形式

---
+++ algorithm/src/main/java/com/sean/base/chapter09/Code03_SmallerEqualBigger.java
@@ -0,0 +1,139 @@
+package com.sean.base.chapter09;
+
+/**
+ * @Auther: xionghaiyang
+ * @Date: 2022-10-24 21:29
+ * @Description: 将单向链表按某值划分成左边小、中间相等、右边大的形式
+ */
+public class Code03_SmallerEqualBigger {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+    }
+
+    public Node listPartition1(Node head, int pivot) {
+        if (head == null) {
+            return head;
+        }
+        Node cur = head;
+        int i = 0;
+        while (cur != null) {
+            i++;
+            cur = cur.next;
+        }
+        Node[] nodeArr = new Node[i];
+        i = 0;
+        cur = head;
+        for (i = 0; i < nodeArr.length; i++) {
+            nodeArr[i] = cur;
+            cur = cur.next;
+        }
+        arrPartition(nodeArr, pivot);
+        for (i = 1; i < nodeArr.length; i++) {
+            nodeArr[i - 1].next = nodeArr[i];
+        }
+        nodeArr[i - 1].next = null;
+        return nodeArr[0];
+    }
+
+    private void arrPartition(Node[] nodeArr, int pivot) {
+        int small = -1;
+        int big = nodeArr.length;
+        int index = 0;
+        while (index != big) {
+            if (nodeArr[index].value < pivot) {
+                swap(nodeArr, ++small, index++);
+            } else if (nodeArr[index].value == pivot) {
+                index++;
+            } else {
+                swap(nodeArr, --big, index);
+            }
+        }
+    }
+
+    private void swap(Node[] nodeArr, int a, int b) {
+        Node tmp = nodeArr[a];
+        nodeArr[a] = nodeArr[b];
+        nodeArr[b] = tmp;
+    }
+
+    public Node listPartition2(Node head, int pivot) {
+        Node sH = null;//small head
+        Node sT = null;//small tail
+        Node eH = null;//equal head
+        Node eT = null;//equal tail
+        Node mH = null;//big head
+        Node mT = null;//big tail
+        Node next = null;//save next node
+        while (head != null) {
+            next = head.next;
+            head.next = null;
+            if (head.value < pivot) {
+                if (sH == null) {
+                    sH = head;
+                    sT = head;
+                } else {
+                    sT.next = head;
+                    sT = head;
+                }
+            } else if (head.value == pivot) {
+                if (eH == null) {
+                    eH = head;
+                    eT = head;
+                } else {
+                    eT.next = head;
+                    eT = head;
+                }
+            } else {
+                if (mH == null) {
+                    mH = head;
+                    mT = head;
+                } else {
+                    mT.next = head;
+                    mT = head;
+                }
+            }
+            head = next;
+        }
+        if (sT != null) {
+            sT.next = eH;
+            eT = eT == null ? sT : eT;
+        }
+        if (eT != null) {
+            eT.next = mH;
+        }
+        return sH != null ? sH : (eH != null ? eH : mH);
+    }
+
+    public void printLinkedList(Node node) {
+        System.out.print("Linked List:");
+        while (node != null) {
+            System.out.print(node.value + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head1 = new Node(7);
+        head1.next = new Node(9);
+        head1.next.next = new Node(1);
+        head1.next.next.next = new Node(8);
+        head1.next.next.next.next = new Node(5);
+        head1.next.next.next.next.next = new Node(2);
+        head1.next.next.next.next.next.next = new Node(5);
+        Code03_SmallerEqualBigger solution = new Code03_SmallerEqualBigger();
+        solution.printLinkedList(head1);
+        head1 = solution.listPartition1(head1, 4);
+        solution.printLinkedList(head1);
+        head1 = solution.listPartition2(head1, 5);
+        solution.printLinkedList(head1);
+    }
+
+}
