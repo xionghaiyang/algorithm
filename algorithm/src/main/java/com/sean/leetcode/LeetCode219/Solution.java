1f48914bfd553f6b4641e9653edddd7e593a2c75
xionghaiyang <18756618469@163.com>
2025-01-29 07:50:03 +0800

    219. 存在重复元素 II

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode219/Solution.java
@@ -0,0 +1,44 @@
+package com.sean.leetcode.LeetCode219;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-01-29 07:42
+ * @Description https://leetcode.cn/problems/contains-duplicate-ii/
+ * 219. 存在重复元素 II
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。
+ * 如果存在，返回 true ；否则，返回 false 。
+ */
+public class Solution {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate1(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
