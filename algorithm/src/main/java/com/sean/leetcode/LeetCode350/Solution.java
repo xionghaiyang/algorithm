a6e4a3ff566494060b1f236f60d71b02a5e5d39e
xionghaiyang <18756618469@163.com>
2025-01-30 08:44:15 +0800

    350. 两个数组的交集 II

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode350/Solution.java
@@ -0,0 +1,46 @@
+package com.sean.leetcode.LeetCode350;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-01-30 08:35
+ * @Description https://leetcode.cn/problems/intersection-of-two-arrays-ii/
+ * 350. 两个数组的交集 II
+ * 给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。
+ * 返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。
+ * 可以不考虑输出结果的顺序。
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ */
+public class Solution {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                map.put(num, map.getOrDefault(num, 0) - 1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+}
