48c2a354649f514dadb7d660cea9078b5f6452bf
xionghaiyang <18756618469@163.com>
2025-02-04 14:40:39 +0800

    922. 按奇偶排序数组 II

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode922/Solution.java
@@ -0,0 +1,38 @@
+package com.sean.leetcode.LeetCode922;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-02-04 14:25
+ * @Description https://leetcode.cn/problems/sort-array-by-parity-ii/
+ * 922. 按奇偶排序数组 II
+ * 给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数 。
+ * 对数组进行排序，以便当 nums[i] 为奇数时，i 也是 奇数 ；当 nums[i] 为偶数时， i 也是 偶数 。
+ * 你可以返回 任何满足上述条件的数组作为答案 。
+ * 2 <= nums.length <= 2 * 10^4
+ * nums.length 是偶数
+ * nums 中一半是偶数
+ * 0 <= nums[i] <= 1000
+ */
+public class Solution {
+
+    public int[] sortArrayByParityII(int[] nums) {
+        int n = nums.length;
+        int j = 1;
+        for (int i = 0; i < n; i += 2) {
+            if (nums[i] % 2 == 1) {
+                while (j < n && nums[j] % 2 == 1) {
+                    j += 2;
+                }
+                swap(nums, i, j);
+            }
+        }
+        return nums;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
