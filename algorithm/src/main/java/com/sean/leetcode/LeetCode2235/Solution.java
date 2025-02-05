5597736c649052485629e1a89e52237249d04ac2
xionghaiyang <18756618469@163.com>
2025-02-05 15:46:47 +0800

    2235. 两整数相加

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode2235/Solution.java
@@ -0,0 +1,16 @@
+package com.sean.leetcode.LeetCode2235;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-02-05 15:32
+ * @Description https://leetcode.cn/problems/add-two-integers
+ * 2235. 两整数相加
+ * 给你两个整数 num1 和 num2，返回这两个整数的和。
+ */
+public class Solution {
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
