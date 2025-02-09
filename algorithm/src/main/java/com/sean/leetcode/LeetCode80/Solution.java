7e2add8649886cd3d2e348e433dd462ba2284ae3
xionghaiyang <18756618469@163.com>
2025-02-09 11:59:10 +0800

    80. 删除有序数组中的重复项 II

--- algorithm/src/main/java/com/sean/leetcode/LeetCode80/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode80/Solution.java
@@ -1,8 +1,5 @@
 package com.sean.leetcode.LeetCode80;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-13 13:49
@@ -10,23 +7,21 @@ import java.util.Map;
  * 80. 删除有序数组中的重复项 II
  * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
  * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * 1 <= nums.length <= 3 * 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 已按升序排列
  */
 public class Solution {
 
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if (map.getOrDefault(nums[i], 0) < 2) {
-                nums[j++] = nums[i];
+        int size = 2;
+        for (int i = 2; i < n; i++) {
+            if (nums[i] != nums[size - 2]) {
+                nums[size++] = nums[i];
             }
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        return j;
+        return Math.min(size, n);
     }
 
 }
