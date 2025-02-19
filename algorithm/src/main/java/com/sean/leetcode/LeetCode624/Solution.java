91dd45480b7adc0d86c9e01dfe8aa4f8dcf7bf60
xionghaiyang <18756618469@163.com>
2025-02-19 09:21:39 +0800

    624. 数组列表中的最大距离

--- algorithm/src/main/java/com/sean/leetcode/LeetCode624/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode624/Solution.java
@@ -25,7 +25,7 @@ public class Solution {
         int res = 0;
         int min = arrays.get(0).get(0);
         int max = arrays.get(0).get(arrays.get(0).size() - 1);
-        for (int i = 1; i < arrays.size(); i++) {
+        for (int i = 1; i < m; i++) {
             res = Math.max(res, Math.max(Math.abs(max - arrays.get(i).get(0)), Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - min)));
             min = Math.min(min, arrays.get(i).get(0));
             max = Math.max(max, arrays.get(i).get(arrays.get(i).size() - 1));
