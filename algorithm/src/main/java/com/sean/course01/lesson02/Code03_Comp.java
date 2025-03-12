eda8e2f126c7be3f4a0e50270721cf2ff51e7d01
xionghaiyang <18756618469@163.com>
2025-03-12 18:11:15 +0800

    对数器

--- algorithm/src/main/java/com/sean/course01/lesson02/Code03_Comp.java
+++ algorithm/src/main/java/com/sean/course01/lesson02/Code03_Comp.java
@@ -1,14 +1,20 @@
-package com.sean.learning01.class02;
+package com.sean.course01.lesson02;
 
+/**
+ * @Author xionghaiyang
+ * @Date 2025-03-12 17:50
+ * @Description 对数器
+ */
 public class Code03_Comp {
 
     public static void selectionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = 0; i < arr.length - 1; i++) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
+            for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
@@ -17,44 +23,46 @@ public class Code03_Comp {
         }
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
-
     public static void insertionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = 1; i < arr.length; i++) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
             for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
                 swap(arr, j, j + 1);
             }
         }
     }
 
-    public static int[] lenRandomValueRandom(int maxLen, int maxValue) {
-        int len = (int) (Math.random() * maxLen);
-        int[] ans = new int[len];
-        for (int i = 0; i < len; i++) {
-            ans[i] = (int) (Math.random() * maxValue);
-        }
-        return ans;
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
-    public static int[] copyArray(int[] arr) {
+    //返回一个数组arr,arr长度[0,maxLen-1],arr中的每个值[0,maxValue-1]
+    private static int[] lenRandomValueRandom(int maxLen, int maxValue) {
+        int len = (int) (Math.random() * maxLen);
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            res[i] = (int) (Math.random() * maxValue);
+        }
+        return res;
+    }
+
+    private static int[] copyArray(int[] arr) {
         if (arr == null) {
             return null;
         }
-        int[] ans = new int[arr.length];
+        int[] res = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            ans[i] = arr[i];
+            res[i] = arr[i];
         }
-        return ans;
+        return res;
     }
 
-    public static boolean isSorted(int[] arr) {
+    private static boolean isSorted(int[] arr) {
         if (arr.length < 2) {
             return true;
         }
@@ -70,22 +78,22 @@ public class Code03_Comp {
 
     public static void main(String[] args) {
         int maxLen = 5;
-        int maxValue = 1000;
+        int maxValue = 100;
         int testTime = 10000;
         for (int i = 0; i < testTime; i++) {
             int[] arr1 = lenRandomValueRandom(maxLen, maxValue);
-            int[] tmp = copyArray(arr1);
-            selectionSort(arr1);
-            if (!isSorted(arr1)) {
-                for(int j = 0;j < tmp.length;j++){
-                    System.out.print(tmp[j]+" ");
+            int[] arr2 = copyArray(arr1);
+            selectionSort(arr2);
+            //insertionSort(arr2);
+            if (!isSorted(arr2)) {
+                for (int j = 0; j < arr1.length; j++) {
+                    System.out.print(arr1[j] + " ");
                 }
                 System.out.println();
-                System.out.println("选择排序错了");
+                System.out.println("排序错了");
                 break;
             }
         }
     }
-
 
 }
