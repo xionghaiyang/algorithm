06a89ce1b701e5808f7fe88738b6d1c68dcc4971
xionghaiyang <18756618469@163.com>
2025-03-25 22:16:07 +0800

    是否是二叉搜索树

--- algorithm/src/main/java/com/sean/course01/lesson07/Code05_IsBinarySearchTree.java
+++ algorithm/src/main/java/com/sean/course01/lesson07/Code05_IsBinarySearchTree.java
@@ -1,18 +1,23 @@
-package com.sean.learning01.class07;
+package com.sean.course01.lesson07;
 
+/**
+ * @Author xionghaiyang
+ * @Date 2025-03-25 21:50
+ * @Description 是否是二叉搜索树
+ */
 public class Code05_IsBinarySearchTree {
 
-    public static class TreeNode {
+    public class TreeNode {
         public int val;
         public TreeNode left;
         public TreeNode right;
 
-        TreeNode(int val) {
+        public TreeNode(int val) {
             this.val = val;
         }
     }
 
-    public static class Info {
+    public class Info {
         public boolean isBST;
         public int max;
         public int min;
@@ -24,7 +29,7 @@ public class Code05_IsBinarySearchTree {
         }
     }
 
-    public static Info process(TreeNode x) {
+    public Info process(TreeNode x) {
         if (x == null) {
             return null;
         }
@@ -43,8 +48,8 @@ public class Code05_IsBinarySearchTree {
         boolean isBST = false;
         boolean leftIsBST = leftInfo == null ? true : leftInfo.isBST;
         boolean rightIsBST = rightInfo == null ? true : rightInfo.isBST;
-        boolean leftMaxLessX = leftInfo == null ? true : (leftInfo.max < max);
-        boolean rightMinMoreX = rightInfo == null ? true : (rightInfo.min > min);
+        boolean leftMaxLessX = leftInfo == null ? true : (leftInfo.max < x.val);
+        boolean rightMinMoreX = rightInfo == null ? true : (rightInfo.min > x.val);
         if (leftIsBST && rightIsBST && leftMaxLessX && rightMinMoreX) {
             isBST = true;
         }
