3fcc3f75083da8c08a12bc294d1ea05018bbcd2f
xionghaiyang <18756618469@163.com>
2025-03-26 22:16:19 +0800

    随机快速排序

--- algorithm/src/main/java/com/sean/course01/lesson08/Code03_PartitionAndQuickSort.java
+++ algorithm/src/main/java/com/sean/course01/lesson08/Code03_PartitionAndQuickSort.java
@@ -1,26 +1,31 @@
-package com.sean.learning01.class08;
+package com.sean.course01.lesson08;
 
-import com.sun.org.apache.regexp.internal.RE;
-import com.sun.xml.internal.ws.addressing.WsaTubeHelperImpl;
-import sun.dc.pr.PRError;
-import sun.util.resources.sq.CalendarData_sq;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-import java.util.Stack;
-
+/**
+ * @Author xionghaiyang
+ * @Date 2025-03-26 20:58
+ * @Description 随机快速排序
+ */
 public class Code03_PartitionAndQuickSort {
 
-    public static void swap(int[] arr, int i, int j) {
+    private static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
 
+    //以arr[n-1]为划分值，将arr分为两个部分
+    //左边小于等于区域，右边大于区域
     public static void splitNum1(int[] arr) {
+        //小于等于区域的右边界
         int lessEqualR = -1;
         int index = 0;
-        int N = arr.length;
-        while (index < N) {
-            if (arr[index] <= arr[N - 1]) {
+        int n = arr.length;
+        while (index < n) {
+            if (arr[index] <= arr[n - 1]) {
+                //与小于等于区域下一个数交换
                 swap(arr, ++lessEqualR, index++);
             } else {
                 index++;
@@ -28,37 +33,46 @@ public class Code03_PartitionAndQuickSort {
         }
     }
 
+    //以arr[n-1]为划分值，将arr分为三个部分
+    //左边为小于区域，中间为等于区域，右边为大于区域
     public static void splitNum2(int[] arr) {
-        int N = arr.length;
+        int n = arr.length;
+        //小于区域右边界
         int lessR = -1;
-        int moreL = N - 1;
+        //大于区域左边界
+        int moreL = n - 1;
         int index = 0;
         while (index < moreL) {
-            if (arr[index] < arr[N - 1]) {
+            if (arr[index] < arr[n - 1]) {
+                //与小于等于区域右边界下一个数交换
                 swap(arr, ++lessR, index++);
-            } else if (arr[index] > arr[N - 1]) {
+            } else if (arr[index] > arr[n - 1]) {
+                //与大于区域左边界下一个数交换
                 swap(arr, --moreL, index);
             } else {
                 index++;
             }
         }
-        swap(arr, moreL, N - 1);
+        swap(arr, moreL, n - 1);
     }
 
-    public static int[] partition(int[] arr, int L, int R) {
-        int lessR = L - 1;
-        int moreL = R;
-        int index = L;
+    //在arr[left...right]范围上，拿arr[right]做划分值
+    //小于区域，等于区域和大于区域
+    //返回等于区域的范围
+    public static int[] partition(int[] arr, int left, int right) {
+        int lessR = left - 1;
+        int moreL = right;
+        int index = left;
         while (index < moreL) {
-            if (arr[index] < arr[R]) {
+            if (arr[index] < arr[right]) {
                 swap(arr, ++lessR, index++);
-            } else if (arr[index] > arr[R]) {
+            } else if (arr[index] > arr[right]) {
                 swap(arr, --moreL, index);
             } else {
                 index++;
             }
         }
-        swap(arr, moreL, R);
+        swap(arr, moreL, right);
         return new int[]{lessR + 1, moreL};
     }
 
@@ -69,64 +83,66 @@ public class Code03_PartitionAndQuickSort {
         process(arr, 0, arr.length - 1);
     }
 
-    public static void process(int[] arr, int L, int R) {
-        if (L >= R) {
+    private static void process(int[] arr, int left, int right) {
+        if (left >= right) {
             return;
         }
-        int[] equalE = partition(arr, L, R);
-        process(arr, L, equalE[0] - 1);
-        process(arr, equalE[1] + 1, R);
+        int[] equalE = partition(arr, left, right);
+        process(arr, left, equalE[0] - 1);
+        process(arr, equalE[1] + 1, right);
     }
 
     public static class Job {
-        public int L;
-        public int R;
+        public int left;
+        public int right;
 
         public Job(int left, int right) {
-            L = left;
-            R = right;
+            this.left = left;
+            this.right = right;
         }
-
     }
 
     public static void quickSort2(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        Stack<Job> stack = new Stack<>();
+        Deque<Job> stack = new ArrayDeque<Job>();
         stack.push(new Job(0, arr.length - 1));
         while (!stack.isEmpty()) {
             Job cur = stack.pop();
-            int[] equals = partition(arr, cur.L, cur.R);
-            if (equals[0] > cur.L) {//有<区域
-                stack.push(new Job(cur.L, equals[0] - 1));
+            int[] equals = partition(arr, cur.left, cur.right);
+            //有小于区域
+            if (equals[0] > cur.left) {
+                stack.push(new Job(cur.left, equals[0] - 1));
             }
-            if (equals[1] < cur.R) {
-                stack.push(new Job(equals[1] + 1, cur.R));
+            //有大于区域
+            if (equals[1] < cur.right) {
+                stack.push(new Job(equals[1] + 1, cur.right));
             }
         }
     }
 
-    public static int[] netherlandsFlag(int[] arr, int L, int R) {
-        if (L > R) {
+    //荷兰国旗问题
+    public static int[] netherlandsFlag(int[] arr, int left, int right) {
+        if (left > right) {
             return new int[]{-1, -1};
         }
-        if (L == R) {
-            return new int[]{L, R};
+        if (left == right) {
+            return new int[]{left, right};
         }
-        int less = L - 1;
-        int more = R;
-        int index = L;
+        int less = left - 1;
+        int more = right;
+        int index = left;
         while (index < more) {
-            if (arr[index] == arr[R]) {
+            if (arr[index] == arr[right]) {
                 index++;
-            } else if (arr[index] < arr[R]) {
-                swap(arr, index++, ++less);
+            } else if (arr[index] < arr[right]) {
+                swap(arr, ++less, index++);
             } else {
-                swap(arr, index, --more);
+                swap(arr, --more, index);
             }
         }
-        swap(arr, more, R);
+        swap(arr, more, right);
         return new int[]{less + 1, more};
     }
 
@@ -137,14 +153,14 @@ public class Code03_PartitionAndQuickSort {
         process3(arr, 0, arr.length - 1);
     }
 
-    public static void process3(int[] arr, int L, int R) {
-        if (L >= R) {
+    private static void process3(int[] arr, int left, int right) {
+        if (left >= right) {
             return;
         }
-        swap(arr, L + (int) (Math.random() * (R - L + 1)), R);
-        int[] equalArea = netherlandsFlag(arr, L, R);
-        process3(arr, L, equalArea[0] - 1);
-        process3(arr, equalArea[1] + 1, R);
+        swap(arr, left + (int) ((right - left + 1) * Math.random()), right);
+        int[] equalArea = netherlandsFlag(arr, left, right);
+        process3(arr, left, equalArea[0] - 1);
+        process3(arr, equalArea[1] + 1, right);
     }
 
     public static int[] generateRandomArray(int maxSize, int maxValue) {
@@ -167,7 +183,7 @@ public class Code03_PartitionAndQuickSort {
     }
 
     public static boolean isEqual(int[] arr1, int[] arr2) {
-        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+        if (arr1 == null ^ arr2 == null) {
             return false;
         }
         if (arr1 == null && arr2 == null) {
@@ -195,11 +211,14 @@ public class Code03_PartitionAndQuickSort {
     }
 
     public static void main(String[] args) {
+        int[] arr = {7, 1, 3, 5, 4, 5, 1, 4, 2, 4, 2, 4};
+        splitNum2(arr);
+        printArray(arr);
         int testTime = 500000;
         int maxSize = 100;
         int maxValue = 100;
         boolean succeed = true;
-        System.out.println("test begin");
+        System.out.println("测试开始");
         for (int i = 0; i < testTime; i++) {
             int[] arr1 = generateRandomArray(maxSize, maxValue);
             int[] arr2 = copyArray(arr1);
@@ -214,10 +233,9 @@ public class Code03_PartitionAndQuickSort {
                 printArray(arr3);
                 break;
             }
-
         }
         System.out.println(succeed ? "Nice!" : "Oops!");
-        System.out.println("test end");
+        System.out.println("测试结束");
     }
 
 }
