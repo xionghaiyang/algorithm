e892e008d3f61a302b7a685b4a8a27197aa64718
xionghaiyang <18756618469@163.com>
2025-03-27 18:18:40 +0800

    在一个有序数组中，找某个数是否存在

---
+++ algorithm/src/main/java/com/sean/course02/lesson01/Code04_BSExist.java
@@ -0,0 +1,66 @@
+package com.sean.course02.lesson01;
+
+import java.util.Arrays;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-03-27 18:01
+ * @Description 在一个有序数组中，找某个数是否存在
+ */
+public class Code04_BSExist {
+
+    public static boolean exist(int[] sortedArr, int num) {
+        if (sortedArr == null || sortedArr.length == 0) {
+            return false;
+        }
+        int left = 0;
+        int right = sortedArr.length - 1;
+        int mid;
+        while (left < right) {
+            mid = left + ((right - left) >> 1);
+            if (sortedArr[mid] == num) {
+                return true;
+            } else if (sortedArr[mid] > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return sortedArr[left] == num;
+    }
+
+    private static boolean test(int[] sortedArr, int num) {
+        for (int cur : sortedArr) {
+            if (cur == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            Arrays.sort(arr);
+            int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            if (test(arr, value) != exist(arr, value)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "ERROR!");
+    }
+
+}
