3fecd9e224f72ae6818dd26fc462cd593bccdc9b
xionghaiyang <18756618469@163.com>
2025-03-27 13:41:21 +0800

    插入排序

--- algorithm/src/main/java/com/sean/course02/lesson01/Code03_InsertionSort.java
+++ algorithm/src/main/java/com/sean/course02/lesson01/Code03_InsertionSort.java
@@ -1,27 +1,34 @@
-package com.sean.learning02.class01;
+package com.sean.course02.lesson01;
 
 import java.util.Arrays;
 
+/**
+ * @Author xionghaiyang
+ * @Date 2025-03-27 13:07
+ * @Description 插入排序
+ */
 public class Code03_InsertionSort {
+
+    //i和j是一个位置的话，会出错
+    private static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
 
     public static void insertionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = 1; i < arr.length; i++) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
             for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
                 swap(arr, j, j + 1);
             }
         }
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
-    }
-
-    public static int[] generateRandomArray(int maxSize, int maxValue) {
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
         int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
@@ -29,7 +36,7 @@ public class Code03_InsertionSort {
         return arr;
     }
 
-    public static int[] copyArray(int[] arr) {
+    private static int[] copyArray(int[] arr) {
         if (arr == null) {
             return null;
         }
@@ -40,8 +47,8 @@ public class Code03_InsertionSort {
         return res;
     }
 
-    public static boolean isEqual(int[] arr1, int[] arr2) {
-        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+    private static boolean isEqual(int[] arr1, int[] arr2) {
+        if (arr1 == null ^ arr2 == null) {
             return false;
         }
         if (arr1 == null && arr2 == null) {
@@ -58,8 +65,8 @@ public class Code03_InsertionSort {
         return true;
     }
 
-    public static void printArray(int[] arr) {
-        if (arr == null || arr.length == 0) {
+    private static void printArray(int[] arr) {
+        if (arr == null) {
             return;
         }
         for (int i = 0; i < arr.length; i++) {
@@ -69,13 +76,12 @@ public class Code03_InsertionSort {
     }
 
     public static void main(String[] args) {
-        int testTime = 50000;
+        int testTime = 500000;
         int maxSize = 100;
         int maxValue = 100;
         boolean succeed = true;
-        System.out.println("test begin!");
         for (int i = 0; i < testTime; i++) {
-            int[] arr = generateRandomArray(maxSize, maxSize);
+            int[] arr = generateRandomArray(maxSize, maxValue);
             int[] arr1 = copyArray(arr);
             int[] arr2 = copyArray(arr);
             insertionSort(arr1);
@@ -86,7 +92,7 @@ public class Code03_InsertionSort {
                 break;
             }
         }
-        System.out.println(succeed ? "Nice!" : "Oops!");
-        System.out.println("test end!");
+        System.out.println(succeed ? "Nice!" : "ERROR!");
     }
+
 }
