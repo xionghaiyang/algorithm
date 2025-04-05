466d586a0eafba548290aa670b62304bf8c99519
xionghaiyang <18756618469@163.com>
2025-04-05 10:28:43 +0800

    给定两棵二叉树的头节点head1和head2 想知道head1中是否有某个子树的结构和head2完全一样

---
+++ algorithm/src/main/java/com/sean/course03/lesson04/Code02_TreeEqual.java
@@ -0,0 +1,176 @@
+package com.sean.course03.lesson04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-04-04 21:44
+ * @Description 给定两棵二叉树的头节点head1和head2
+ * 想知道head1中是否有某个子树的结构和head2完全一样
+ */
+public class Code02_TreeEqual {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    //big做头节点的树，其中是否有棵子树的结构是和small为头的树，完全一样的
+    public static boolean containsTree1(Node big, Node small) {
+        if (small == null) {
+            return true;
+        }
+        //small != null
+        if (big == null) {
+            return false;
+        }
+        //big != null && small != null
+        if (isSameValueStructure(big, small)) {
+            return true;
+        }
+        return containsTree1(big.left, small) || containsTree1(big.right, small);
+    }
+
+    //head1为头的树，是否在结构对应上，完全和head2一样
+    private static boolean isSameValueStructure(Node head1, Node head2) {
+        if (head1 == null ^ head2 == null) {
+            return false;
+        }
+        if (head1 == null && head2 == null) {
+            return true;
+        }
+        if (head1.value != head2.value) {
+            return false;
+        }
+        return isSameValueStructure(head1.left, head2.left) && isSameValueStructure(head1.right, head2.right);
+    }
+
+    public static boolean containsTree2(Node big, Node small) {
+        if (small == null) {
+            return true;
+        }
+        if (big == null) {
+            return false;
+        }
+        List<String> b = preSerial(big);
+        List<String> s = preSerial(small);
+        String[] str = new String[b.size()];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = b.get(i);
+        }
+        String[] match = new String[s.size()];
+        for (int i = 0; i < match.length; i++) {
+            match[i] = s.get(i);
+        }
+        return getIndexOf(str, match) != -1;
+    }
+
+    private static List<String> preSerial(Node head) {
+        List<String> res = new ArrayList<>();
+        pres(head, res);
+        return res;
+    }
+
+    private static void pres(Node head, List<String> res) {
+        if (head == null) {
+            res.add(null);
+        } else {
+            res.add(String.valueOf(head.value));
+            pres(head.left, res);
+            pres(head.right, res);
+        }
+    }
+
+    private static int getIndexOf(String[] str1, String[] str2) {
+        if (str1 == null || str2 == null || str1.length < 1 || str1.length < str2.length) {
+            return -1;
+        }
+        int x = 0;
+        int y = 0;
+        int[] next = getNextArray(str2);
+        while (x < str1.length && y < str2.length) {
+            if (isEqual(str1[x], str2[y])) {
+                x++;
+                y++;
+            } else if (next[y] == -1) {
+                x++;
+            } else {
+                y = next[y];
+            }
+        }
+        return y == str2.length ? x - y : -1;
+    }
+
+    private static int[] getNextArray(String[] ms) {
+        if (ms.length == -1) {
+            return new int[]{-1};
+        }
+        int[] next = new int[ms.length];
+        next[0] = -1;
+        next[1] = 0;
+        int i = 2;
+        int cn = 0;
+        while (i < next.length) {
+            if (isEqual(ms[i - 1], ms[cn])) {
+                next[i++] = ++cn;
+            } else if (cn > 0) {
+                cn = next[cn];
+            } else {
+                next[i++] = 0;
+            }
+        }
+        return next;
+    }
+
+    private static boolean isEqual(String a, String b) {
+        if (a == null && b == null) {
+            return true;
+        } else {
+            if (a == null || b == null) {
+                return false;
+            } else {
+                return a.equals(b);
+            }
+        }
+    }
+
+    private static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    private static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (maxValue * Math.random()));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int bigTreeLevel = 7;
+        int smallTreeLevel = 4;
+        int nodeMaxValue = 5;
+        int testTimes = 100000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTimes; i++) {
+            Node big = generateRandomBST(bigTreeLevel, nodeMaxValue);
+            Node small = generateRandomBST(smallTreeLevel, nodeMaxValue);
+            boolean res1 = containsTree1(big, small);
+            boolean res2 = containsTree2(big, small);
+            if (res1 != res2) {
+                System.out.println("出错了");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
