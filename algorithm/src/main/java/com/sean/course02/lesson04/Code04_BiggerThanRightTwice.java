77dddfcf5cd84419ec7ae653b01db8a6288fc114
xionghaiyang <18756618469@163.com>
2025-04-16 20:59:14 +0800

    数组中存在“数对”满足左边的数比右边的数2倍还要大，求这样的“数对”种数

---
+++ algorithm/src/main/java/com/sean/course02/lesson04/Code04_BiggerThanRightTwice.java
@@ -0,0 +1,112 @@
+package com.sean.course02.lesson04;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-04-16 20:02
+ * @Description 数组中存在“数对”满足左边的数比右边的数2倍还要大，求这样的“数对”种数
+ */
+public class Code04_BiggerThanRightTwice {
+
+    public static int biggerTwice(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        return process(arr, 0, arr.length - 1);
+    }
+
+    private static int process(int[] arr, int left, int right) {
+        if (left == right) {
+            return 0;
+        }
+        int mid = left + ((right - left) >> 1);
+        return process(arr, left, mid) + process(arr, mid + 1, right) + merge(arr, left, mid, right);
+    }
+
+    private static int merge(int[] arr, int left, int mid, int right) {
+        int res = 0;
+        //目前囊括进来的数，是[mid+1,windowR)
+        int windowR = mid + 1;
+        for (int i = left; i <= mid; i++) {
+            while (windowR <= right && arr[i] > (arr[windowR] << 1)) {
+                windowR++;
+            }
+            res += windowR - mid - 1;
+        }
+        int[] help = new int[right - left + 1];
+        int i = 0;
+        int p1 = left;
+        int p2 = mid + 1;
+        while (p1 <= mid && p2 <= right) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= mid) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= right) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[left + i] = help[i];
+        }
+        return res;
+    }
+
+    public static int comparator(int[] arr) {
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > (arr[j] << 1)) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    private static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return arr;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    private static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            if (biggerTwice(arr1) != comparator(arr2)) {
+                System.out.println("出错了");
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
