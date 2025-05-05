a275974ba8b9ddcfb626a38e81fa8a5025bc1de8
xionghaiyang <18756618469@163.com>
2025-05-05 21:06:05 +0800

    前缀树

---
+++ algorithm/src/main/java/com/sean/course02/lesson08/Code01_TrieTree.java
@@ -0,0 +1,316 @@
+package com.sean.course02.lesson08;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-05-05 19:47
+ * @Description 前缀树
+ * 1）单个字符串中，字符从前到后的加到一棵多叉树上
+ * 2）字符放在路上，节点上有专属的数据项（常见的是pass和end值）
+ * 3）所有样本都这样添加，如果没有路就新建，如有路就复用
+ * 4）沿途节点的pass值增加1，每个字符串结束时来到的节点end值增加1
+ * 可以完成前缀相关的查询
+ * 设计一种结构。用户可以：
+ * 1）void insert(String str)            添加某个字符串，可以重复添加，每次算1个
+ * 2）int search(String str)             查询某个字符串在结构中还有几个
+ * 3)  void delete(String str)           删掉某个字符串，可以重复删除，每次算1个
+ * 4）int prefixNumber(String str)  查询有多少个字符串，是以str做前缀的
+ */
+public class Code01_TrieTree {
+
+    public static class Node1 {
+        public int pass;
+        public int end;
+        public Node1[] nexts;
+
+        public Node1() {
+            pass = 0;
+            end = 0;
+            //nexts[i] == null i方向的路不存在
+            //nexts[i] != null i方向的路存在
+            nexts = new Node1[26];
+        }
+    }
+
+    public static class Trie1 {
+        private Node1 root;
+
+        public Trie1() {
+            root = new Node1();
+        }
+
+        public void insert(String word) {
+            if (word == null) {
+                return;
+            }
+            char[] str = word.toCharArray();
+            Node1 node = root;
+            node.pass++;
+            int index;
+            for (int i = 0; i < str.length; i++) {
+                index = str[i] - 'a';
+                if (node.nexts[index] == null) {
+                    node.nexts[index] = new Node1();
+                }
+                node = node.nexts[index];
+                node.pass++;
+            }
+            node.end++;
+        }
+
+        public void delete(String word) {
+            if (search(word) != 0) {
+                char[] chs = word.toCharArray();
+                Node1 node = root;
+                node.pass--;
+                int index;
+                for (int i = 0; i < chs.length; i++) {
+                    index = chs[i] - 'a';
+                    if (--node.nexts[index].pass == 0) {
+                        node.nexts[index] = null;
+                        return;
+                    }
+                    node = node.nexts[index];
+                }
+                node.end--;
+            }
+        }
+
+        //word这个单词之前加入过几次
+        public int search(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] chs = word.toCharArray();
+            Node1 node = root;
+            int index;
+            for (int i = 0; i < chs.length; i++) {
+                index = chs[i] - 'a';
+                if (node.nexts[index] == null) {
+                    return 0;
+                }
+                node = node.nexts[index];
+            }
+            return node.end;
+        }
+
+        //所有加入的字符串中，有几个是以pre这个字符串作为前缀的
+        public int prefixNumber(String pre) {
+            if (pre == null) {
+                return 0;
+            }
+            char[] chs = pre.toCharArray();
+            Node1 node = root;
+            int index;
+            for (int i = 0; i < chs.length; i++) {
+                index = chs[i] - 'a';
+                if (node.nexts[index] == null) {
+                    return 0;
+                }
+                node = node.nexts[index];
+            }
+            return node.pass;
+        }
+    }
+
+    public static class Node2 {
+        public int pass;
+        public int end;
+        public Map<Integer, Node2> nexts;
+
+        public Node2() {
+            pass = 0;
+            end = 0;
+            nexts = new HashMap<>();
+        }
+    }
+
+    public static class Trie2 {
+        private Node2 root;
+
+        public Trie2() {
+            root = new Node2();
+        }
+
+        public void insert(String word) {
+            if (word == null) {
+                return;
+            }
+            char[] chs = word.toCharArray();
+            Node2 node = root;
+            node.pass++;
+            int index;
+            for (int i = 0; i < chs.length; i++) {
+                index = (int) chs[i];
+                if (!node.nexts.containsKey(index)) {
+                    node.nexts.put(index, new Node2());
+                }
+                node = node.nexts.get(index);
+                node.pass++;
+            }
+            node.end++;
+        }
+
+        public void delete(String word) {
+            if (search(word) != 0) {
+                char[] chs = word.toCharArray();
+                Node2 node = root;
+                node.pass--;
+                int index;
+                for (int i = 0; i < chs.length; i++) {
+                    index = (int) chs[i];
+                    if (--node.nexts.get(index).pass == 0) {
+                        node.nexts.remove(index);
+                        return;
+                    }
+                    node = node.nexts.get(index);
+                }
+                node.end--;
+            }
+        }
+
+        //word这个单词之前加入过几次
+        public int search(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] chs = word.toCharArray();
+            Node2 node = root;
+            int index;
+            for (int i = 0; i < chs.length; i++) {
+                index = (int) chs[i];
+                if (!node.nexts.containsKey(index)) {
+                    return 0;
+                }
+                node = node.nexts.get(index);
+            }
+            return node.end;
+        }
+
+        //所有的字符串中，有几个是以pre这个字符串作为前缀的
+        public int prefixNumber(String pre) {
+            if (pre == null) {
+                return 0;
+            }
+            char[] chs = pre.toCharArray();
+            Node2 node = root;
+            int index;
+            for (int i = 0; i < chs.length; i++) {
+                index = (int) chs[i];
+                if (!node.nexts.containsKey(index)) {
+                    return 0;
+                }
+                node = node.nexts.get(index);
+            }
+            return node.pass;
+        }
+    }
+
+    public static class Right {
+        private HashMap<String, Integer> box;
+
+        public Right() {
+            box = new HashMap<>();
+        }
+
+        public void insert(String word) {
+            if (!box.containsKey(word)) {
+                box.put(word, 1);
+            } else {
+                box.put(word, box.get(word) + 1);
+            }
+        }
+
+        public void delete(String word) {
+            if (box.containsKey(word)) {
+                if (box.get(word) == 1) {
+                    box.remove(word);
+                } else {
+                    box.put(word, box.get(word) - 1);
+                }
+            }
+        }
+
+        public int search(String word) {
+            if (!box.containsKey(word)) {
+                return 0;
+            } else {
+                return box.get(word);
+            }
+        }
+
+        public int prefixNumber(String pre) {
+            int count = 0;
+            for (String cur : box.keySet()) {
+                if (cur.startsWith(pre)) {
+                    count += box.get(cur);
+                }
+            }
+            return count;
+        }
+
+    }
+
+    private static String generateRandomString(int strLen) {
+        char[] res = new char[(int) (strLen * Math.random()) + 1];
+        for (int i = 0; i < res.length; i++) {
+            int value = (int) (6 * Math.random());
+            res[i] = (char) (97 + value);
+        }
+        return String.valueOf(res);
+    }
+
+    private static String[] generateRandomStringArray(int arrLen, int strLen) {
+        String[] res = new String[(int) (arrLen * Math.random()) + 1];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = generateRandomString(strLen);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int arrLen = 100;
+        int strLen = 20;
+        int testTimes = 100000;
+        System.out.println("测试开始");
+        out:
+        for (int i = 0; i < testTimes; i++) {
+            String[] arr = generateRandomStringArray(arrLen, strLen);
+            Trie1 trie1 = new Trie1();
+            Trie2 trie2 = new Trie2();
+            Right right = new Right();
+            for (int j = 0; j < arr.length; j++) {
+                double decide = Math.random();
+                if (decide < 0.25) {
+                    trie1.insert(arr[j]);
+                    trie2.insert(arr[j]);
+                    right.insert(arr[j]);
+                } else if (decide < 0.5) {
+                    trie1.delete(arr[j]);
+                    trie2.delete(arr[j]);
+                    right.delete(arr[j]);
+                } else if (decide < 0.75) {
+                    int res1 = trie1.search(arr[j]);
+                    int res2 = trie2.search(arr[j]);
+                    int res3 = right.search(arr[j]);
+                    if (res1 != res3 || res2 != res3) {
+                        System.out.println("出错了");
+                        break out;
+                    }
+                } else {
+                    int res1 = trie1.prefixNumber(arr[j]);
+                    int res2 = trie2.prefixNumber(arr[j]);
+                    int res3 = right.prefixNumber(arr[j]);
+                    if (res1 != res3 || res2 != res3) {
+                        System.out.println("出错了");
+                        break out;
+                    }
+                }
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
