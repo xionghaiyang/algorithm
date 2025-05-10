9e14f2bee5416790503608224991e7a4cb5a8e0c
xionghaiyang <18756618469@163.com>
2025-05-10 12:51:54 +0800

    二叉树的最大宽度

---
+++ algorithm/src/main/java/com/sean/course02/lesson11/Code05_TreeMaxWidth.java
@@ -0,0 +1,141 @@
+package com.sean.course02.lesson11;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-05-10 11:31
+ * @Description 二叉树的最大宽度
+ */
+public class Code05_TreeMaxWidth {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static int maxWidthUseMap(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(head);
+        Map<Node, Integer> levelMap = new HashMap<>();
+        levelMap.put(head, 1);
+        int curLevel = 1;
+        int curLevelNodes = 0;
+        int max = 0;
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            int curNodeLevel = levelMap.get(cur);
+            if (cur.left != null) {
+                levelMap.put(cur.left, curNodeLevel + 1);
+                queue.offer(cur.left);
+            }
+            if (cur.right != null) {
+                levelMap.put(cur.right, curNodeLevel + 1);
+                queue.offer(cur.right);
+            }
+            if (curNodeLevel == curLevel) {
+                curLevelNodes++;
+            } else {
+                max = Math.max(max, curLevelNodes);
+                curLevel++;
+                curLevelNodes = 1;
+            }
+        }
+        max = Math.max(max, curLevelNodes);
+        return max;
+    }
+
+    public static int maxWidthNoMap(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(head);
+        Node curEnd = head;
+        Node nextEnd = null;
+        int max = 0;
+        int curLevelNodes = 0;
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            if (cur.left != null) {
+                queue.offer(cur.left);
+                nextEnd = cur.left;
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+                nextEnd = cur.right;
+            }
+            curLevelNodes++;
+            if (cur == curEnd) {
+                max = Math.max(max, curLevelNodes);
+                curLevelNodes = 0;
+                curEnd = nextEnd;
+            }
+        }
+        return max;
+    }
+
+    public static int maxWidth(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(head);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            res = Math.max(res, size);
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+        return res;
+    }
+
+    private static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    private static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (maxValue * Math.random()));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 10;
+        int maxValue = 10;
+        int testTimes = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (maxWidthUseMap(head) != maxWidthNoMap(head) || maxWidthUseMap(head) != maxWidth(head)) {
+                System.out.println("出错了");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
