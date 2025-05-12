73b8c75f452b12b7847676189c317ae34cc8ba0c
xionghaiyang <18756618469@163.com>
2025-05-12 22:34:13 +0800

    是否是完全二叉树

---
+++ algorithm/src/main/java/com/sean/course02/lesson12/Code01_IsCBT.java
@@ -0,0 +1,132 @@
+package com.sean.course02.lesson12;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-05-12 21:35
+ * @Description 是否是完全二叉树
+ */
+public class Code01_IsCBT {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static boolean isCBT1(Node head) {
+        if (head == null) {
+            return true;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        //是否遇到过左右两个孩子不双全的节点
+        boolean leaf = false;
+        Node left = null;
+        Node right = null;
+        queue.offer(head);
+        while (!queue.isEmpty()) {
+            head = queue.poll();
+            left = head.left;
+            right = head.right;
+            //如果遇到了不双全的节点之后，又发现当前节点不是叶节点
+            if ((leaf && (left != null || right != null)) || (left == null && right != null)) {
+                return false;
+            }
+            if (left != null) {
+                queue.offer(left);
+            }
+            if (right != null) {
+                queue.offer(right);
+            }
+            if (left == null || right == null) {
+                leaf = true;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isCBT2(Node head) {
+        if (head == null) {
+            return true;
+        }
+        return process(head).isCBT;
+    }
+
+    public static class Info {
+        //是否是满二叉树
+        public boolean isFull;
+        //是否是完全二叉树
+        public boolean isCBT;
+        //高度
+        public int height;
+
+        public Info(boolean isFull, boolean isCBT, int height) {
+            this.isFull = isFull;
+            this.isCBT = isCBT;
+            this.height = height;
+        }
+    }
+
+    private static Info process(Node head) {
+        if (head == null) {
+            return new Info(true, true, 0);
+        }
+        Info leftInfo = process(head.left);
+        Info rightInfo = process(head.right);
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        boolean isFull = leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height;
+        boolean isCBT = false;
+        if (isFull) {
+            isCBT = true;
+        } else {
+            if (leftInfo.isCBT && rightInfo.isCBT) {
+                if (leftInfo.isCBT && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+                    isCBT = true;
+                }
+                if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+                    isCBT = true;
+                }
+                if (leftInfo.isFull && rightInfo.isCBT && leftInfo.height == rightInfo.height) {
+                    isCBT = true;
+                }
+            }
+        }
+        return new Info(isFull, isCBT, height);
+    }
+
+    private static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    private static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (maxValue * Math.random()));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 5;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isCBT1(head) != isCBT2(head)) {
+                System.out.println("出错了");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
