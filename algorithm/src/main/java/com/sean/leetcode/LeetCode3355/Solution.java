747916eca8802b3f74baba54679f6f5a9678f918
xionghaiyang <18756618469@163.com>
2025-05-20 08:43:23 +0800

    3355. 零数组变换 I

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode3355/Solution.java
@@ -0,0 +1,56 @@
+package com.sean.leetcode.LeetCode3355;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-05-20 06:29
+ * @Description https://leetcode.cn/problems/zero-array-transformation-i
+ * 3355. 零数组变换 I
+ * 给定一个长度为 n 的整数数组 nums 和一个二维数组 queries，其中 queries[i] = [li, ri]。
+ * 对于每个查询 queries[i]：
+ * 在 nums 的下标范围 [li, ri] 内选择一个下标 子集。
+ * 将选中的每个下标对应的元素值减 1。
+ * 零数组 是指所有元素都等于 0 的数组。
+ * 如果在按顺序处理所有查询后，可以将 nums 转换为 零数组 ，则返回 true，否则返回 false。
+ * 1 <= nums.length <= 10^5
+ * 0 <= nums[i] <= 10^5
+ * 1 <= queries.length <= 10^5
+ * queries[i].length == 2
+ * 0 <= li <= ri < nums.length
+ */
+public class Solution {
+
+    //超时 O(NM)
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        for (int[] query : queries) {
+            int left = query[0];
+            int right = query[1];
+            for (int i = left; i <= right; i++) {
+                nums[i]--;
+            }
+        }
+        for (int num : nums) {
+            if (num > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isZeroArray1(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] diff = new int[n + 1];
+        for (int[] query : queries) {
+            diff[query[0]]++;
+            diff[query[1] + 1]--;
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += diff[i];
+            if (nums[i] > sum) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
