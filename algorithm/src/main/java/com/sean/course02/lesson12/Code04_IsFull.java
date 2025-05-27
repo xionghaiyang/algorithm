e6a08d6a2d6364da915217e402cc142c1650a314
xionghaiyang <18756618469@163.com>
2025-05-27 12:15:45 +0800

    是否是完全二叉树

---
+++ algorithm/src/main/java/com/sean/course02/lesson12/Code04_IsFull.java
@@ -0,0 +1,101 @@
+package com.sean.course02.lesson12;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-05-27 11:55
+ * @Description 是否是完全二叉树
+ */
+public class Code04_IsFull {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static boolean isFull1(Node head) {
+        if (head == null) {
+            return true;
+        }
+        int height = h(head);
+        int nodes = n(head);
+        return (1 << height) - 1 == nodes;
+    }
+
+    private static int h(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        return Math.max(h(head.left), h(head.right)) + 1;
+    }
+
+    private static int n(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        return n(head.left) + n(head.right) + 1;
+    }
+
+    public static boolean isFull2(Node head) {
+        if (head == null) {
+            return true;
+        }
+        Info info = process(head);
+        return (1 << info.height) - 1 == info.nodes;
+    }
+
+    public static class Info {
+        public int height;
+        public int nodes;
+
+        public Info(int height, int nodes) {
+            this.height = height;
+            this.nodes = nodes;
+        }
+    }
+
+    public static Info process(Node head) {
+        if (head == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = process(head.left);
+        Info rightInfo = process(head.right);
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        int nodes = leftInfo.nodes + rightInfo.nodes + 1;
+        return new Info(height, nodes);
+    }
+
+    private static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    private static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (maxValue * Math.random()));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 5;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isFull1(head) != isFull2(head)) {
+                System.out.println("出错了!");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
