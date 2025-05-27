d80867cfe19fd35e1b8e08e52ccbfc387e70718e
xionghaiyang <18756618469@163.com>
2025-05-27 13:25:55 +0800

    最大二叉搜索子树的大小

---
+++ algorithm/src/main/java/com/sean/course02/lesson12/Code05_MaxSubBSTSize.java
@@ -0,0 +1,149 @@
+package com.sean.course02.lesson12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-05-27 12:16
+ * @Description 最大二叉搜索子树的大小
+ */
+public class Code05_MaxSubBSTSize {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static int maxSubBSTSize1(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        int h = getBSTSize(head);
+        if (h != 0) {
+            return h;
+        }
+        return Math.max(maxSubBSTSize1(head.left), maxSubBSTSize1(head.right));
+    }
+
+    private static int getBSTSize(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        List<Node> arr = new ArrayList<>();
+        in(head, arr);
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i).value <= arr.get(i - 1).value) {
+                return 0;
+            }
+        }
+        return arr.size();
+    }
+
+    private static void in(Node head, List<Node> arr) {
+        if (head == null) {
+            return;
+        }
+        in(head.left, arr);
+        arr.add(head);
+        in(head.right, arr);
+    }
+
+    public static int maxSubBSTSize2(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        return process(head).maxBSTSubtreeSize;
+    }
+
+    public static class Info {
+        public int maxBSTSubtreeSize;
+        public int allSize;
+        public int max;
+        public int min;
+
+        public Info(int maxBSTSubtreeSize, int allSize, int max, int min) {
+            this.maxBSTSubtreeSize = maxBSTSubtreeSize;
+            this.allSize = allSize;
+            this.max = max;
+            this.min = min;
+        }
+    }
+
+    private static Info process(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Info leftInfo = process(head.left);
+        Info rightInfo = process(head.right);
+        int max = head.value;
+        int min = head.value;
+        int allSize = 1;
+        if (leftInfo != null) {
+            max = Math.max(leftInfo.max, max);
+            min = Math.min(leftInfo.min, min);
+            allSize += leftInfo.allSize;
+        }
+        if (rightInfo != null) {
+            max = Math.max(rightInfo.max, max);
+            min = Math.min(rightInfo.min, min);
+            allSize += rightInfo.allSize;
+        }
+        int p1 = -1;
+        if (leftInfo != null) {
+            p1 = leftInfo.maxBSTSubtreeSize;
+        }
+        int p2 = -1;
+        if (rightInfo != null) {
+            p2 = rightInfo.maxBSTSubtreeSize;
+        }
+        int p3 = -1;
+        boolean leftBST = leftInfo == null || leftInfo.maxBSTSubtreeSize == leftInfo.allSize;
+        boolean rightBST = rightInfo == null || rightInfo.maxBSTSubtreeSize == rightInfo.allSize;
+        if (leftBST && rightBST) {
+            boolean leftMaxLessHead = leftInfo == null || leftInfo.max < head.value;
+            boolean rightMinMoreHead = rightInfo == null || head.value < rightInfo.min;
+            if (leftMaxLessHead && rightMinMoreHead) {
+                int leftSize = leftInfo == null ? 0 : leftInfo.allSize;
+                int rightSize = rightInfo == null ? 0 : rightInfo.allSize;
+                p3 = leftSize + rightSize + 1;
+            }
+        }
+        return new Info(Math.max(p1, Math.max(p2, p3)), allSize, max, min);
+    }
+
+    private static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    private static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (maxValue * Math.random()));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 4;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (maxSubBSTSize1(head) != maxSubBSTSize2(head)) {
+                System.out.println("出错了!");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
