215ec3535bd490775ba9ea19bad32c747f740282
xionghaiyang <18756618469@163.com>
2025-05-27 18:16:18 +0800

    1. 两数之和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1/Solution.java
@@ -12,12 +12,17 @@ import java.util.Map;
  * 并返回它们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
  * 你可以按任意顺序返回答案。
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * 只会存在一个有效答案
  */
 public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
             int key = target - nums[i];
             if (map.containsKey(key)) {
                 return new int[]{map.get(key), i};
