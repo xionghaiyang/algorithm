90bf9fa63ad5a1dd36eca8992711c27f938e6229
xionghaiyang <18756618469@163.com>
2025-05-27 18:27:12 +0800

    49. 字母异位词分组

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode49/Solution.java
@@ -0,0 +1,55 @@
+package com.sean.leetcode.LeetCode49;
+
+import java.util.*;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-05-27 18:16
+ * @Description https://leetcode.cn/problems/group-anagrams
+ * 49. 字母异位词分组
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。
+ * 可以按任意顺序返回结果列表。
+ * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] 仅包含小写字母
+ */
+public class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] count = new int[26];
+            int n = str.length();
+            for (int i = 0; i < n; i++) {
+                count[str.charAt(i) - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                if (count[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(count[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+}
