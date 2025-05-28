dce0b64188c512c0f68ebe3d0e1cf60ba4693e9c
xionghaiyang <18756618469@163.com>
2025-05-28 09:22:19 +0800

    15. 三数之和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode15/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode15/Solution.java
@@ -12,6 +12,8 @@ import java.util.List;
  * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
  * 你返回所有和为 0 且不重复的三元组。
  * 注意：答案中不可以包含重复的三元组。
+ * 3 <= nums.length <= 3000
+ * -10^5 <= nums[i] <= 10^5
  */
 public class Solution {
 
