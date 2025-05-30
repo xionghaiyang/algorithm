170a040277ee6ab0b659ee73057f61dff288276e
xionghaiyang <18756618469@163.com>
2025-05-30 08:45:24 +0800

    560. 和为 K 的子数组

--- algorithm/src/main/java/com/sean/leetcode/LeetCode560/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode560/Solution.java
@@ -10,6 +10,9 @@ import java.util.Map;
  * 560. 和为 K 的子数组
  * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
  * 子数组是数组中元素的连续非空序列。
+ * 1 <= nums.length <= 2 * 10^4
+ * -1000 <= nums[i] <= 1000
+ * -10^7 <= k <= 10^7
  */
 public class Solution {
 
