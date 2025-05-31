decc62fe96ba558001e7f00a7dd67d4cb014b42f
xionghaiyang <18756618469@163.com>
2025-05-31 09:03:18 +0800

    53. 最大子数组和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode53/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode53/Solution.java
@@ -7,13 +7,12 @@ package com.sean.leetcode.LeetCode53;
  * 53. 最大子数组和
  * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
  * 子数组是数组中的一个连续部分。
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
  */
 public class Solution {
 
     public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         int res = nums[0];
         int sum = 0;
         for (int num : nums) {
