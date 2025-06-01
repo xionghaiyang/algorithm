4a524d85a9872ec610e163fcea8158d469d97df6
xionghaiyang <18756618469@163.com>
2025-06-02 06:58:41 +0800

    240. 搜索二维矩阵 II

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode240/Solution.java
@@ -0,0 +1,37 @@
+package com.sean.leetcode.LeetCode240;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-06-02 06:48
+ * @Description https://leetcode.cn/problems/search-a-2d-matrix-ii
+ * 240. 搜索二维矩阵 II
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。
+ * 该矩阵具有以下特性：
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= n, m <= 300
+ * -10^9 <= matrix[i][j] <= 10^9
+ * 每行的所有元素从左到右升序排列
+ * 每列的所有元素从上到下升序排列
+ * -10^9 <= target <= 10^9
+ */
+public class Solution {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+}
