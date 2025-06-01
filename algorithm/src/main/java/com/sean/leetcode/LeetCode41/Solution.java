cb16a09f56e69366a053c84a4b71977a800b38de
xionghaiyang <18756618469@163.com>
2025-06-01 09:46:27 +0800

    41. 缺失的第一个正数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode41/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode41/Solution.java
@@ -10,10 +10,12 @@ import java.util.Set;
  * 41. 缺失的第一个正数
  * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
  * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ * 1 <= nums.length <= 10^5
+ * -2^31 <= nums[i] <= 2^31 - 1
  */
 public class Solution {
 
-    public int firstMissingPositive1(int[] nums) {
+    public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
@@ -27,13 +29,11 @@ public class Solution {
         return n + 1;
     }
 
-    public int firstMissingPositive(int[] nums) {
+    public int firstMissingPositive1(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                int temp = nums[nums[i] - 1];
-                nums[nums[i] - 1] = nums[i];
-                nums[i] = temp;
+            while (1 <= nums[i] && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
             }
         }
         for (int i = 0; i < n; i++) {
@@ -42,6 +42,12 @@ public class Solution {
             }
         }
         return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
 }
