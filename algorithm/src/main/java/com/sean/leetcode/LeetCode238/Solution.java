d898eacb721f1bc55039e406625b25ab483a0b4d
xionghaiyang <18756618469@163.com>
2025-06-01 09:31:46 +0800

    238. 除自身以外数组的乘积

--- algorithm/src/main/java/com/sean/leetcode/LeetCode238/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode238/Solution.java
@@ -8,6 +8,9 @@ package com.sean.leetcode.LeetCode238;
  * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
  * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
  * 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ * 2 <= nums.length <= 10^5
+ * -30 <= nums[i] <= 30
+ * 输入 保证 数组 answer[i] 在  32 位 整数范围内
  */
 public class Solution {
 
@@ -26,6 +29,21 @@ public class Solution {
         int[] res = new int[n];
         for (int i = 0; i < n; i++) {
             res[i] = left[i] * right[i];
+        }
+        return res;
+    }
+
+    public int[] productExceptSelf1(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = nums[i - 1] * res[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
         }
         return res;
     }
