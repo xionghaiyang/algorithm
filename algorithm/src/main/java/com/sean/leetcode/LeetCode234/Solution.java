968996133a85a94840252e3bf85f2edb688e1278
xionghaiyang <18756618469@163.com>
2025-06-03 10:52:50 +0800

    234. 回文链表

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode234/Solution.java
@@ -0,0 +1,96 @@
+package com.sean.leetcode.LeetCode234;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-06-03 10:03
+ * @Description https://leetcode.cn/problems/palindrome-linked-list
+ * 234. 回文链表
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。
+ * 如果是，返回 true ；否则，返回 false 。
+ * 链表中节点数目在范围[1, 10^5] 内
+ * 0 <= Node.val <= 9
+ */
+public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode cur = head;
+        while (cur != null) {
+            list.add(cur.val);
+            cur = cur.next;
+        }
+        int left = 0, right = list.size() - 1;
+        while (left < right) {
+            if (list.get(left) != list.get(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome1(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        //找到前半部分链表的尾节点并反转后半部分链表
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverse(firstHalfEnd.next);
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean res = true;
+        while (res && p2 != null) {
+            if (p1.val != p2.val) {
+                res = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        firstHalfEnd.next = reverse(secondHalfStart);
+        return res;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}
