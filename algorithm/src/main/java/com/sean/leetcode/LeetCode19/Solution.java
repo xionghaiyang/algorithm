f270d22e8830c4e27cea3ee70e9c8ee2b41f95c8
xionghaiyang <18756618469@163.com>
2025-06-05 08:15:02 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode19/Solution.java
@@ -0,0 +1,52 @@
+package com.sean.leetcode.LeetCode19;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-06-05 08:08
+ * @Description https://leetcode.cn/problems/remove-nth-node-from-end-of-list
+ * 19. 删除链表的倒数第 N 个结点
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 链表中结点的数目为 sz
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ */
+public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+
+}
