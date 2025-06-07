9a7bfd325217dae00d9b770b7ea544c76890b442
xionghaiyang <18756618469@163.com>
2025-06-07 10:17:41 +0800

    104. 二叉树的最大深度

--- algorithm/src/main/java/com/sean/leetcode/LeetCode104/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode104/Solution.java
@@ -7,6 +7,8 @@ package com.sean.leetcode.LeetCode104;
  * 104. 二叉树的最大深度
  * 给定一个二叉树 root ，返回其最大深度。
  * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ * 树中节点的数量在 [0, 10^4] 区间内。
+ * -100 <= Node.val <= 100
  */
 public class Solution {
 
