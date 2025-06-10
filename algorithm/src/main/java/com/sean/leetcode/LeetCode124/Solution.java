d330dbce9f2acdaa24bf5dc8e3386ae6d0d477cd
xionghaiyang <18756618469@163.com>
2025-06-10 22:31:14 +0800

    124. 二叉树中的最大路径和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode124/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode124/Solution.java
@@ -3,13 +3,15 @@ package com.sean.leetcode.LeetCode124;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-09-27 20:15
- * @Description: https://leetcode.cn/problems/binary-tree-maximum-path-sum/?plan=zhijigangwei&plan_progress=stbyva7
+ * @Description: https://leetcode.cn/problems/binary-tree-maximum-path-sum
  * 124. 二叉树中的最大路径和
  * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。
  * 同一个节点在一条路径序列中 至多出现一次 。
  * 该路径 至少包含一个 节点，且不一定经过根节点。
  * 路径和 是路径中各节点值的总和。
  * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 树中节点数目范围是 [1, 3 * 10^4]
+ * -1000 <= Node.val <= 100
  */
 public class Solution {
 
@@ -35,9 +37,6 @@ public class Solution {
     int max = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
         process(root);
         return max;
     }
