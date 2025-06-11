d517aaa12552c6267714bd68b8453bc9503f2333
xionghaiyang <18756618469@163.com>
2025-06-12 06:59:20 +0800

    46. 全排列

--- algorithm/src/main/java/com/sean/leetcode/LeetCode46/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode46/Solution.java
@@ -10,36 +10,37 @@ import java.util.List;
  * 46. 全排列
  * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。
  * 你可以 按任意顺序 返回答案。
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
  */
 public class Solution {
 
-    List<List<Integer>> res = new ArrayList<>();
-    List<Integer> temp = new ArrayList<>();
-    int n;
+    private List<List<Integer>> res = new ArrayList<>();
+    private List<Integer> temp = new ArrayList<>();
+    private int n;
+    private boolean[] visited;
 
     public List<List<Integer>> permute(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return res;
-        }
         n = nums.length;
-        boolean[] visit = new boolean[n];
-        dfs(nums, visit, 0);
+        visited = new boolean[n];
+        process(nums);
         return res;
     }
 
-    private void dfs(int[] nums, boolean[] visit, int size) {
-        if (size == n) {
+    private void process(int[] nums) {
+        if (temp.size() == n) {
             res.add(new ArrayList<>(temp));
             return;
         }
         for (int i = 0; i < n; i++) {
-            if (visit[i]) {
+            if (visited[i]) {
                 continue;
             }
-            visit[i] = true;
+            visited[i] = true;
             temp.add(nums[i]);
-            dfs(nums, visit, size + 1);
-            visit[i] = false;
+            process(nums);
+            visited[i] = false;
             temp.remove(temp.size() - 1);
         }
     }
