bca3b66bd7f05e0665e3548c662ef232631ef577
xionghaiyang <18756618469@163.com>
2025-06-11 20:42:46 +0800

    1486. 数组异或操作

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1486/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1486/Solution.java
@@ -8,6 +8,9 @@ package com.sean.leetcode.LeetCode1486;
  * 给你两个整数，n 和 start 。
  * 数组 nums 定义为：nums[i] = start + 2*i（下标从 0 开始）且 n == nums.length 。
  * 请返回 nums 中所有元素按位异或（XOR）后得到的结果。
+ * 1 <= n <= 1000
+ * 0 <= start <= 1000
+ * n == nums.length
  */
 public class Solution {
 
