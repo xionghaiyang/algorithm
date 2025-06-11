c2e0bb55ab33b73452e6097d119752bce4829556
xionghaiyang <18756618469@163.com>
2025-06-11 13:16:06 +0800

    709. 转换成小写字母

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode709/Solution.java
@@ -0,0 +1,18 @@
+package com.sean.leetcode.LeetCode709;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-06-11 13:11
+ * @Description https://leetcode.cn/problems/to-lower-case
+ * 709. 转换成小写字母
+ * 给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+ * 1 <= s.length <= 100
+ * s 由 ASCII 字符集中的可打印字符组成
+ */
+public class Solution {
+
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+}
