74b3964f6f19c919d0ac8b384cd8804c6f63823d
xionghaiyang <18756618469@163.com>
2025-06-11 23:40:13 +0800

    面试题 01.01. 判定字符是否唯一

--- algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview0101/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview0101/Solution.java
@@ -1,26 +1,26 @@
 package com.sean.leetcode.LeetCodeInterview0101;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-09-23 12:16
- * @Description: https://leetcode.cn/problems/is-unique-lcci/?favorite=xb9lfcwi
+ * @Description: https://leetcode.cn/problems/is-unique-lcci
  * 面试题 01.01. 判定字符是否唯一
  * 实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
+ * 0 <= len(s) <= 100
+ * s[i]仅包含小写字母
+ * 如果你不使用额外的数据结构，会很加分。
  */
 public class Solution {
 
     public boolean isUnique(String astr) {
-        Set<Character> set = new HashSet<>();
-        int n = astr.length();
-        for (int i = 0; i < n; i++) {
-            char c = astr.charAt(i);
-            if (set.contains(c)) {
+        int mask = 0;
+        for (char c : astr.toCharArray()) {
+            int cur = 1 << (c - 'a');
+            if ((mask & cur) != 0) {
                 return false;
+            } else {
+                mask |= cur;
             }
-            set.add(c);
         }
         return true;
     }
