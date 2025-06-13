c3b7aa31b1b00bf1f1462b61b9694598f534c178
xionghaiyang <18756618469@163.com>
2025-06-14 06:38:50 +0800

    35. 搜索插入位置

--- algorithm/src/main/java/com/sean/leetcode/LeetCode35/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode35/Solution.java
@@ -8,6 +8,10 @@ package com.sean.leetcode.LeetCode35;
  * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
  * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
  * 请必须使用时间复杂度为 O(log n) 的算法。
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 为 无重复元素 的 升序 排列数组
+ * -10^4 <= target <= 10^4
  */
 public class Solution {
 
