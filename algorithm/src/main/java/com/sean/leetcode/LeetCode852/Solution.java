06d6de3b57093f1b11642ff6724f79199c93eb9f
xionghaiyang <18756618469@163.com>
2025-06-13 13:39:12 +0800

    852. 山脉数组的峰顶索引

--- algorithm/src/main/java/com/sean/leetcode/LeetCode852/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode852/Solution.java
@@ -13,6 +13,9 @@ package com.sean.leetcode.LeetCode852;
  * 给你由整数组成的山脉数组 arr ，
  * 返回满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] 的下标 i 。
  * 你必须设计并实现时间复杂度为 O(log(n)) 的解决方案。
+ * 3 <= arr.length <= 10^5
+ * 0 <= arr[i] <= 10^6
+ * 题目数据 保证 arr 是一个山脉数组
  */
 public class Solution {
 
@@ -20,7 +23,7 @@ public class Solution {
         int n = arr.length;
         int left = 1, right = n - 2, res = 0;
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = left + ((right - left) >> 1);
             if (arr[mid] > arr[mid + 1]) {
                 res = mid;
                 right = mid - 1;
