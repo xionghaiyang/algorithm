c5dffea30e19002b28870bc6979a143b273b36d7
xionghaiyang <18756618469@163.com>
2025-06-13 13:22:25 +0800

    1422. 分割字符串的最大得分

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1422/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1422/Solution.java
@@ -8,32 +8,14 @@ package com.sean.leetcode.LeetCode1422;
  * 给你一个由若干 0 和 1 组成的字符串 s ，请你计算并返回将该字符串
  * 分割成两个 非空 子字符串（即 左 子字符串和 右 子字符串）所能获得的最大得分。
  * 「分割字符串的得分」为 左 子字符串中 0 的数量加上 右 子字符串中 1 的数量。
+ * 2 <= s.length <= 500
+ * 字符串 s 仅由字符 '0' 和 '1' 组成。
  */
 public class Solution {
 
-    public int maxScore1(String s) {
-        int res = 0;
-        int n = s.length();
-        for (int i = 1; i < n; i++) {
-            int score = 0;
-            for (int j = 0; j < i; j++) {
-                if (s.charAt(j) == '0') {
-                    score++;
-                }
-            }
-            for (int j = i; j < n; j++) {
-                if (s.charAt(j) == '1') {
-                    score++;
-                }
-            }
-            res = Math.max(res, score);
-        }
-        return res;
-    }
-
     public int maxScore(String s) {
-        int score = 0;
         int n = s.length();
+        int score = 0;
         if (s.charAt(0) == '0') {
             score++;
         }
@@ -49,7 +31,7 @@ public class Solution {
             } else {
                 score--;
             }
-            res = Math.max(score, res);
+            res = Math.max(res, score);
         }
         return res;
     }
