aff768a72a4af0dcd60fc4802126daab1140fa2f
xionghaiyang <18756618469@163.com>
2025-06-13 13:15:41 +0800

    1470. 重新排列数组

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1470/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1470/Solution.java
@@ -7,6 +7,9 @@ package com.sean.leetcode.LeetCode1470;
  * 1470. 重新排列数组
  * 给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
  * 请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+ * 1 <= n <= 500
+ * nums.length == 2n
+ * 1 <= nums[i] <= 10^3
  */
 public class Solution {
 
