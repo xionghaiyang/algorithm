8c4612db3e8af235197e0afbdb89d1244729d59a
xionghaiyang <18756618469@163.com>
2025-06-14 10:40:50 +0800

    74. 搜索二维矩阵

--- algorithm/src/main/java/com/sean/leetcode/LeetCode74/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode74/Solution.java
@@ -9,15 +9,20 @@ package com.sean.leetcode.LeetCode74;
  * 每行中的整数从左到右按非严格递增顺序排列。
  * 每行的第一个整数大于前一行的最后一个整数。
  * 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 100
+ * -10^4 <= matrix[i][j], target <= 10^4
  */
 public class Solution {
 
+    //O(log(MN))
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
         int left = 0, right = m * n - 1;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + ((right - left) >> 1);
             int x = matrix[mid / n][mid % n];
             if (x < target) {
                 left = mid + 1;
@@ -25,6 +30,23 @@ public class Solution {
                 right = mid - 1;
             } else {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    //O(M+N)
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
             }
         }
         return false;
