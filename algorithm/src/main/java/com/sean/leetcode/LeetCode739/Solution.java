ed3b52ca7babcb4a4940be749c7e723a5c558729
xionghaiyang <18756618469@163.com>
2025-06-16 10:03:50 +0800

    739. 每日温度

--- algorithm/src/main/java/com/sean/leetcode/LeetCode739/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode739/Solution.java
@@ -11,12 +11,15 @@ import java.util.Deque;
  * 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，
  * 其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。
  * 如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ * 1 <= temperatures.length <= 10^5
+ * 30 <= temperatures[i] <= 100
  */
 public class Solution {
 
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         int[] res = new int[n];
+        //大  ->  小
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
