461d8f83dd9a79326b7b94719449e04f13d5f15b
xionghaiyang <18756618469@163.com>
2025-06-18 06:52:21 +0800

    121. 买卖股票的最佳时机

--- algorithm/src/main/java/com/sean/leetcode/LeetCode121/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode121/Solution.java
@@ -3,11 +3,15 @@ package com.sean.leetcode.LeetCode121;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-15 19:47
- * @Description: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock
  * 121. 买卖股票的最佳时机
  * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
- * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
- * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。
+ * 设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。
+ * 如果你不能获取任何利润，返回 0 。
+ * 1 <= prices.length <= 10^5
+ * 0 <= prices[i] <= 10^4
  */
 public class Solution {
 
