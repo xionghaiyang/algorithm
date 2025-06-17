8fb06dcb421e47d9f7a926221cebefa78d9a769b
xionghaiyang <18756618469@163.com>
2025-06-18 06:58:15 +0800

    55. 跳跃游戏

--- algorithm/src/main/java/com/sean/leetcode/LeetCode55/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode55/Solution.java
@@ -3,11 +3,13 @@ package com.sean.leetcode.LeetCode55;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-09-27 19:55
- * @Description: https://leetcode.cn/problems/jump-game/?plan=zhijigangwei&plan_progress=stbyva7
+ * @Description: https://leetcode.cn/problems/jump-game
  * 55. 跳跃游戏
  * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
  * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
  * 判断你是否能够到达最后一个下标。
+ * 1 <= nums.length <= 10^4
+ * 0 <= nums[i] <= 10^5
  */
 public class Solution {
 
