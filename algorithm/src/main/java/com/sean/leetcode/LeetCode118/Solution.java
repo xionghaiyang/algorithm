88b968c347e138d82194eaa6f350d31f60fd9140
xionghaiyang <18756618469@163.com>
2025-06-19 07:24:59 +0800

    118. 杨辉三角

--- algorithm/src/main/java/com/sean/leetcode/LeetCode118/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode118/Solution.java
@@ -6,10 +6,11 @@ import java.util.List;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-28 16:34
- * @Description: https://leetcode.cn/problems/pascals-triangle/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/pascals-triangle
  * 118. 杨辉三角
  * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
  * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ * 1 <= numRows <= 30
  */
 public class Solution {
 
