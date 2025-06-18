f413f64634b5a2eeee7b26cc374325c19fde4f07
xionghaiyang <18756618469@163.com>
2025-06-19 07:38:39 +0800

    198. 打家劫舍

--- algorithm/src/main/java/com/sean/leetcode/LeetCode198/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode198/Solution.java
@@ -1,20 +1,21 @@
 package com.sean.leetcode.LeetCode198;
 
+import java.util.Arrays;
+
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-14 19:54
- * @Description: https://leetcode.cn/problems/house-robber/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/house-robber
  * 198. 打家劫舍
  * 你是一个专业的小偷，计划偷窃沿街的房屋。
  * 每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
  * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
  */
 public class Solution {
 
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         int n = nums.length;
         if (n == 1) {
             return nums[0];
@@ -26,6 +27,28 @@ public class Solution {
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
         return dp[n - 1];
+    }
+
+    public int rob1(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return process(nums, dp, 0);
+    }
+
+    private int process(int[] nums, int[] dp, int i) {
+        if (i >= nums.length) {
+            return 0;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        //偷取
+        int res = nums[i] + process(nums, dp, i + 2);
+        //不偷取
+        res = Math.max(res, process(nums, dp, i + 1));
+        dp[i] = res;
+        return res;
     }
 
 }
