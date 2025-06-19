a83aa5dde37084c5a9f1286d3de13a975b46b892
xionghaiyang <18756618469@163.com>
2025-06-19 08:38:44 +0800

    279. 完全平方数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode279/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode279/Solution.java
@@ -5,38 +5,39 @@ import java.util.Arrays;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-29 14:19
- * @Description: https://leetcode.cn/problems/perfect-squares/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/perfect-squares
  * 279. 完全平方数
  * 给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
  * 完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。
  * 例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+ * 1 <= n <= 10^4
  */
 public class Solution {
 
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        int res = process(n, dp);
+        int res = process(dp, n);
         return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    private int process(int cur, int[] dp) {
-        if (dp[cur] != -1) {
-            return dp[cur];
+    private int process(int[] dp, int i) {
+        if (dp[i] != -1) {
+            return dp[i];
         }
-        if (cur == 0) {
-            dp[cur] = 0;
-            return dp[cur];
+        if (i == 0) {
+            dp[i] = 0;
+            return dp[i];
         }
         int res = Integer.MAX_VALUE;
-        for (int i = 1; i * i <= cur; i++) {
-            res = Math.min(res, process(cur - i * i, dp));
+        for (int j = 1; j * j <= i; j++) {
+            res = Math.min(res, process(dp, i - j * j));
         }
         if (res != Integer.MAX_VALUE) {
             res++;
         }
-        dp[cur] = res;
-        return res;
+        dp[i] = res;
+        return dp[i];
     }
 
 }
