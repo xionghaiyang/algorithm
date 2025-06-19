813aba24255d59c6d37d4c22bad3b3ae2188f089
xionghaiyang <18756618469@163.com>
2025-06-20 07:43:17 +0800

    322. 零钱兑换

--- algorithm/src/main/java/com/sean/leetcode/LeetCode322/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode322/Solution.java
@@ -5,21 +5,22 @@ import java.util.Arrays;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-29 13:18
- * @Description: https://leetcode.cn/problems/coin-change/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/coin-change
  * 322. 零钱兑换
  * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
- * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。
+ * 如果没有任何一种硬币组合能组成总金额，返回 -1 。
  * 你可以认为每种硬币的数量是无限的。
+ * 1 <= coins.length <= 12
+ * 1 <= coins[i] <= 2^31 - 1
+ * 0 <= amount <= 10^4
  */
 public class Solution {
 
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || coins.length == 0) {
-            return -1;
-        }
         int n = coins.length;
         int[][] dp = new int[n + 1][amount + 1];
-        for (int i = 0; i < n + 1; i++) {
+        for (int i = 0; i <= n; i++) {
             Arrays.fill(dp[i], -1);
         }
         int res = process(coins, 0, amount, dp);
