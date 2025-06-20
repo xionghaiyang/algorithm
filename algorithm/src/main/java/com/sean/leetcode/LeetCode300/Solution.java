9d72f792b2e043d729536cc684170c56ab800434
xionghaiyang <18756618469@163.com>
2025-06-20 18:25:00 +0800

    300. 最长递增子序列

--- algorithm/src/main/java/com/sean/leetcode/LeetCode300/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode300/Solution.java
@@ -3,18 +3,17 @@ package com.sean.leetcode.LeetCode300;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-29 10:46
- * @Description: https://leetcode.cn/problems/longest-increasing-subsequence/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/longest-increasing-subsequence
  * 300. 最长递增子序列
  * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
  * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
  * 例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ * 1 <= nums.length <= 2500
+ * -10^4 <= nums[i] <= 10^4
  */
 public class Solution {
 
     public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         int n = nums.length;
         int[] dp = new int[n];
         dp[0] = 1;
@@ -22,7 +21,7 @@ public class Solution {
         for (int i = 1; i < n; i++) {
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
+                if (nums[j] < nums[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
