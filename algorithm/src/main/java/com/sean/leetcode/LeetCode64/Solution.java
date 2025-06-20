fe77cb4921676627dc04d4e837cd5626896d3ddc
xionghaiyang <18756618469@163.com>
2025-06-20 21:13:24 +0800

    64. 最小路径和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode64/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode64/Solution.java
@@ -5,43 +5,49 @@ import java.util.Arrays;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-29 09:48
- * @Description: https://leetcode.cn/problems/minimum-path-sum/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/minimum-path-sum
  * 64. 最小路径和
  * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
  * 说明：每次只能向下或者向右移动一步。
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 200
  */
 public class Solution {
 
+    private int[][] grid;
+    private int m;
+    private int n;
+
     public int minPathSum(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
-            return 0;
-        }
-        int m = grid.length;
-        int n = grid[0].length;
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
             Arrays.fill(dp[i], -1);
         }
-        return process(grid, m - 1, n - 1, dp);
+        return process(0, 0, dp);
     }
 
-    private int process(int[][] grid, int i, int j, int[][] dp) {
+    private int process(int i, int j, int[][] dp) {
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
-        if (i == 0 && j == 0) {
+        if (i == m - 1 && j == n - 1) {
             dp[i][j] = grid[i][j];
             return dp[i][j];
         }
         int res = Integer.MAX_VALUE;
-        if (i - 1 >= 0) {
-            res = Math.min(res, grid[i][j] + process(grid, i - 1, j, dp));
+        if (i < m - 1) {
+            res = Math.min(res, process(i + 1, j, dp));
         }
-        if (j - 1 >= 0) {
-            res = Math.min(res, grid[i][j] + process(grid, i, j - 1, dp));
+        if (j < n - 1) {
+            res = Math.min(res, process(i, j + 1, dp));
         }
-        dp[i][j] = res;
-        return res;
+        dp[i][j] = res + grid[i][j];
+        return dp[i][j];
     }
 
 }
