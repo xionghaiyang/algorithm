be15abe76e4283da8328f468ab18b567b2af21a2
xionghaiyang <18756618469@163.com>
2025-06-20 20:40:08 +0800

    62. 不同路径

--- algorithm/src/main/java/com/sean/leetcode/LeetCode62/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode62/Solution.java
@@ -1,30 +1,46 @@
 package com.sean.leetcode.LeetCode62;
 
+import java.util.Arrays;
+
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-28 17:36
- * @Description: https://leetcode.cn/problems/unique-paths/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/unique-paths
  * 62. 不同路径
  * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
- * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * 机器人每次只能向下或者向右移动一步。
+ * 机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
  * 问总共有多少条不同的路径？
+ * 1 <= m, n <= 100
+ * 题目数据保证答案小于等于 2 * 10^9
  */
 public class Solution {
 
     public int uniquePaths(int m, int n) {
+        //dp[i][j],从(i,j)走到(m-1,n-1)有多少条路径
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
+            Arrays.fill(dp[i], -1);
         }
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
-        return dp[m - 1][n - 1];
+        return process(m, n, 0, 0, dp);
     }
+
+    private int process(int m, int n, int i, int j, int[][] dp) {
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (i == m - 1) {
+            dp[i][j] = 1;
+            return dp[i][j];
+        }
+        if (j == n - 1) {
+            dp[i][j] = 1;
+            return dp[i][j];
+        }
+        int res = process(m, n, i + 1, j, dp) + process(m, n, i, j + 1, dp);
+        dp[i][j] = res;
+        return dp[i][j];
+    }
+
 
 }
