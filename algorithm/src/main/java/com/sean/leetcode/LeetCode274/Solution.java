20df30f7cd2345bba7a453e66cad63655aa7357f
xionghaiyang <18756618469@163.com>
2025-06-21 12:54:52 +0800

    274. H 指数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode274/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode274/Solution.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-13 14:17
- * @Description: https://leetcode.cn/problems/h-index/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/h-index
  * 274. H 指数
  * 给你一个整数数组 citations ，其中 citations[i] 表示研究者的第 i 篇论文被引用的次数。
  * 计算并返回该研究者的 h 指数。
@@ -13,6 +13,9 @@ import java.util.Arrays;
  * 一名科研人员的 h 指数 是指他（她）至少发表了 h 篇论文，
  * 并且 至少 有 h 篇论文被引用次数大于等于 h 。
  * 如果 h 有多种可能的值，h 指数 是其中最大的那个。
+ * n == citations.length
+ * 1 <= n <= 5000
+ * 0 <= citations[i] <= 1000
  */
 public class Solution {
 
