37821de3e3644f64d5327599ff1dc2921c77a326
xionghaiyang <18756618469@163.com>
2025-06-22 08:59:03 +0800

    530. 二叉搜索树的最小绝对差

--- algorithm/src/main/java/com/sean/leetcode/LeetCode530/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode530/Solution.java
@@ -3,10 +3,12 @@ package com.sean.leetcode.LeetCode530;
 /**
  * @Auther: xionghaiyang
  * @Date: 2024-04-13 21:54
- * @Description: https://leetcode.cn/problems/minimum-absolute-difference-in-bst/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/minimum-absolute-difference-in-bst
  * 530. 二叉搜索树的最小绝对差
  * 给你一个二叉搜索树的根节点 root ，返回 树中任意两不同节点值之间的最小差值 。
  * 差值是一个正数，其数值等于两值之差的绝对值。
+ * 树中节点的数目范围是 [2, 10^4]
+ * 0 <= Node.val <= 10^5
  */
 public class Solution {
 
@@ -29,28 +31,40 @@ public class Solution {
         }
     }
 
-    private int res;
-    private int pre;
-
     public int getMinimumDifference(TreeNode root) {
-        res = Integer.MAX_VALUE;
-        pre = -1;
-        dfs(root);
-        return res;
+        return process(root).minDiff;
     }
 
-    private void dfs(TreeNode root) {
+    public class Info {
+        public int min;
+        public int max;
+        public int minDiff;
+
+        public Info(int min, int max, int minDiff) {
+            this.min = min;
+            this.max = max;
+            this.minDiff = minDiff;
+        }
+    }
+
+    private Info process(TreeNode root) {
         if (root == null) {
-            return;
+            return null;
         }
-        dfs(root.left);
-        if (pre == -1) {
-            pre = root.val;
-        } else {
-            res = Math.min(res, root.val - pre);
-            pre = root.val;
+        int min = root.val, max = root.val, minDiff = Integer.MAX_VALUE;
+        Info leftInfo = process(root.left);
+        Info rightInfo = process(root.right);
+        if (leftInfo != null) {
+            min = Math.min(min, leftInfo.min);
+            minDiff = Math.min(minDiff, leftInfo.minDiff);
+            minDiff = Math.min(minDiff, root.val - leftInfo.max);
         }
-        dfs(root.right);
+        if (rightInfo != null) {
+            max = Math.max(max, rightInfo.max);
+            minDiff = Math.min(minDiff, rightInfo.minDiff);
+            minDiff = Math.min(minDiff, rightInfo.min - root.val);
+        }
+        return new Info(min, max, minDiff);
     }
 
 }
