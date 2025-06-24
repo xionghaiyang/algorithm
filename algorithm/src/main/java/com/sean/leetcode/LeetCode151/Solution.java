ac0f525cdeeb53055db6bf77017afd9792502b79
xionghaiyang <18756618469@163.com>
2025-06-24 20:53:21 +0800

    151. 反转字符串中的单词

--- algorithm/src/main/java/com/sean/leetcode/LeetCode151/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode151/Solution.java
@@ -3,7 +3,7 @@ package com.sean.leetcode.LeetCode151;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-05-29 16:16
- * @Description: https://leetcode.cn/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75
+ * @Description: https://leetcode.cn/problems/reverse-words-in-a-string
  * 151. 反转字符串中的单词
  * 给你一个字符串 s ，请你反转字符串中 单词 的顺序。
  * 单词 是由非空格字符组成的字符串。
@@ -11,6 +11,9 @@ package com.sean.leetcode.LeetCode151;
  * 返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
  * 注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。
  * 返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+ * 1 <= s.length <= 10^4
+ * s 包含英文大小写字母、数字和空格 ' '
+ * s 中 至少存在一个 单词
  */
 public class Solution {
 
