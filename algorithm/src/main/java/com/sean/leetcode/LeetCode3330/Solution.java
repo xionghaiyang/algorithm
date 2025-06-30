64e670e7ede3271f6c1e9dafee1d9fe6b0ac2187
xionghaiyang <18756618469@163.com>
2025-07-01 06:24:13 +0800

    3330. 找到初始输入字符串 I

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode3330/Solution.java
@@ -0,0 +1,28 @@
+package com.sean.leetcode.LeetCode3330;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-07-01 05:58
+ * @Description https://leetcode.cn/problems/find-the-original-typed-string-i
+ * 3330. 找到初始输入字符串 I
+ * Alice 正在她的电脑上输入一个字符串。但是她打字技术比较笨拙，她 可能 在一个按键上按太久，导致一个字符被输入 多次 。
+ * 尽管 Alice 尽可能集中注意力，她仍然可能会犯错 至多 一次。
+ * 给你一个字符串 word ，它表示 最终 显示在 Alice 显示屏上的结果。
+ * 请你返回 Alice 一开始可能想要输入字符串的总方案数。
+ * 1 <= word.length <= 100
+ * word 只包含小写英文字母。
+ */
+public class Solution {
+
+    public int possibleStringCount(String word) {
+        int n = word.length();
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+}
