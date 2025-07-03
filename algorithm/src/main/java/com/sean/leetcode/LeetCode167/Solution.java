55d6773016a81806d491f077b6a1d4a47a0b91be
xionghaiyang <18756618469@163.com>
2025-07-03 19:55:09 +0800

    167. 两数之和 II - 输入有序数组

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode167/Solution.java
@@ -0,0 +1,36 @@
+package com.sean.leetcode.LeetCode167;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-07-03 19:47
+ * @Description https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted
+ * 167. 两数之和 II - 输入有序数组
+ * 给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。
+ * 如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
+ * 以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+ * 你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+ * 你所设计的解决方案必须只使用常量级的额外空间。
+ * 2 <= numbers.length <= 3 * 10^4
+ * -1000 <= numbers[i] <= 1000
+ * numbers 按 非递减顺序 排列
+ * -1000 <= target <= 1000
+ * 仅存在一个有效答案
+ */
+public class Solution {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return null;
+    }
+
+}
