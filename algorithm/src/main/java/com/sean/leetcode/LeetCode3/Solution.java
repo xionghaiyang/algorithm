ef85c4f4cb2d05518271cac8cd5c3b2df172a1ec
xionghaiyang <18756618469@163.com>
2025-07-04 09:59:41 +0800

    3. 无重复字符的最长子串

--- algorithm/src/main/java/com/sean/leetcode/LeetCode3/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode3/Solution.java
@@ -6,7 +6,7 @@ import java.util.Set;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-04-21 08:28
- * @Description: https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+ * @Description: https://leetcode.cn/problems/longest-substring-without-repeating-characters
  * 3. 无重复字符的最长子串
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
  * 0 <= s.length <= 5 * 10^4
@@ -19,14 +19,11 @@ public class Solution {
         int n = s.length();
         int res = 0;
         for (int left = 0, right = -1; left < n; left++) {
-            if (left > 0) {
-                set.remove(s.charAt(left - 1));
-            }
             while (right + 1 < n && !set.contains(s.charAt(right + 1))) {
-                set.add(s.charAt(right + 1));
-                right++;
+                set.add(s.charAt(++right));
             }
             res = Math.max(res, right - left + 1);
+            set.remove(s.charAt(left));
         }
         return res;
     }
