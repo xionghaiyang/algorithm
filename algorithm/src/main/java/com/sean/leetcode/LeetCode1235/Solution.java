241367c59b2f2fc53a3c66200e0481c93720f715
xionghaiyang <18756618469@163.com>
2025-07-08 07:24:04 +0800

    1235. 规划兼职工作

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1235/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1235/Solution.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 /**
  * @Author xionghaiyang
  * @Date 2024-05-04 09:39
- * @Description https://leetcode.cn/problems/maximum-profit-in-job-scheduling/
+ * @Description https://leetcode.cn/problems/maximum-profit-in-job-scheduling
  * 1235. 规划兼职工作
  * 你打算利用空闲时间来做兼职工作赚些零花钱。
  * 这里有 n 份兼职工作，每份工作预计从 startTime[i] 开始到 endTime[i] 结束，报酬为 profit[i]。
  * 给你一份兼职工作表，包含开始时间 startTime，结束时间 endTime 和预计报酬 profit 三个数组，请你计算并返回可以获得的最大报酬。
  * 注意，时间上出现重叠的 2 份工作不能同时进行。
  * 如果你选择的工作在时间 X 结束，那么你可以立刻进行在时间 X 开始的下一份工作。
+ * 1 <= startTime.length == endTime.length == profit.length <= 5 * 10^4
+ * 1 <= startTime[i] < endTime[i] <= 10^9
+ * 1 <= profit[i] <= 10^4
  */
 public class Solution {
 
@@ -23,9 +26,9 @@ public class Solution {
         }
         Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
         int[] dp = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            int k = binarySearch(jobs, i - 1, jobs[i - 1][0]);
-            dp[i] = Math.max(dp[i - 1], dp[k] + jobs[i - 1][2]);
+        for (int i = 0; i < n; i++) {
+            int k = binarySearch(jobs, i, jobs[i][0]);
+            dp[i + 1] = Math.max(dp[i], dp[k] + jobs[i][2]);
         }
         return dp[n];
     }
@@ -33,7 +36,7 @@ public class Solution {
     private int binarySearch(int[][] jobs, int right, int target) {
         int left = 0;
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + ((right - left) >> 1);
             if (jobs[mid][1] > target) {
                 right = mid;
             } else {
