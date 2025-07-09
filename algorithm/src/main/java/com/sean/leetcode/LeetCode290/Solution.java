ba8748582cad8a10764cbe11bf122b4b7bf97bab
xionghaiyang <18756618469@163.com>
2025-07-10 06:45:17 +0800

    290. 单词规律

--- algorithm/src/main/java/com/sean/leetcode/LeetCode290/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode290/Solution.java
@@ -1,35 +1,47 @@
 package com.sean.leetcode.LeetCode290;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-23 20:51
- * @Description: https://leetcode.cn/problems/word-pattern/description/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/word-pattern
  * 290. 单词规律
  * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
  * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * 1 <= pattern.length <= 300
+ * pattern 只包含小写英文字母
+ * 1 <= s.length <= 3000
+ * s 只包含小写英文字母和 ' '
+ * s 不包含 任何前导或尾随对空格
+ * s 中每个单词都被 单个空格 分隔
  */
 public class Solution {
 
     public boolean wordPattern(String pattern, String s) {
-        Map<Character, String> map = new HashMap<>();
         String[] arr = s.split(" ");
         if (pattern.length() != arr.length) {
             return false;
         }
+        Map<Character, String> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
         int n = pattern.length();
         for (int i = 0; i < n; i++) {
-            if (!map.containsKey(pattern.charAt(i))) {
-                if (map.containsValue(arr[i])) {
+            char c = pattern.charAt(i);
+            String str = arr[i];
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(str)) {
                     return false;
                 }
-                map.put(pattern.charAt(i), arr[i]);
             } else {
-                if (!map.get(pattern.charAt(i)).equals(arr[i])) {
+                if (set.contains(str)) {
                     return false;
                 }
+                map.put(c, str);
+                set.add(str);
             }
         }
         return true;
