b1bd874bdd8fd651d7d0b714303f83446d627791
xionghaiyang <18756618469@163.com>
2025-07-09 12:25:40 +0800

    383. 赎金信

--- algorithm/src/main/java/com/sean/leetcode/LeetCode383/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode383/Solution.java
@@ -3,11 +3,13 @@ package com.sean.leetcode.LeetCode383;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-23 19:42
- * @Description: https://leetcode.cn/problems/ransom-note/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/ransom-note
  * 383. 赎金信
  * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
  * 如果可以，返回 true ；否则返回 false 。
  * magazine 中的每个字符只能在 ransomNote 中使用一次。
+ * 1 <= ransomNote.length, magazine.length <= 10^5
+ * ransomNote 和 magazine 由小写英文字母组成
  */
 public class Solution {
 
@@ -16,14 +18,11 @@ public class Solution {
             return false;
         }
         int[] cnt = new int[26];
-        int n = magazine.length();
-        for (int i = 0; i < n; i++) {
-            cnt[magazine.charAt(i) - 'a']++;
+        for (char c : magazine.toCharArray()) {
+            cnt[c - 'a']++;
         }
-        int m = ransomNote.length();
-        for (int i = 0; i < m; i++) {
-            cnt[ransomNote.charAt(i) - 'a']--;
-            if (cnt[ransomNote.charAt(i) - 'a'] < 0) {
+        for (char c : ransomNote.toCharArray()) {
+            if (--cnt[c - 'a'] < 0) {
                 return false;
             }
         }
