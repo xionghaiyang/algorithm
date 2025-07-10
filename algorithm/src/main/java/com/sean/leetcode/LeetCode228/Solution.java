6bd8a752604952c932590f7ff64136ccbd9fdd3f
xionghaiyang <18756618469@163.com>
2025-07-10 15:17:45 +0800

    228. 汇总区间

--- algorithm/src/main/java/com/sean/leetcode/LeetCode228/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode228/Solution.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-23 21:35
- * @Description: https://leetcode.cn/problems/summary-ranges/description/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/summary-ranges
  * 228. 汇总区间
  * 给定一个  无重复元素 的 有序 整数数组 nums 。
  * 返回 恰好覆盖数组中所有数字 的 最小有序 区间范围列表 。
@@ -14,6 +14,10 @@ import java.util.List;
  * 列表中的每个区间范围 [a,b] 应该按如下格式输出：
  * "a->b" ，如果 a != b
  * "a" ，如果 a == b
+ * 0 <= nums.length <= 20
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * nums 中的所有值都 互不相同
+ * nums 按升序排列
  */
 public class Solution {
 
@@ -22,16 +26,15 @@ public class Solution {
         int n = nums.length;
         int i = 0;
         while (i < n) {
-            int low = i;
+            int left = i;
             i++;
             while (i < n && nums[i] == nums[i - 1] + 1) {
                 i++;
             }
-            int high = i - 1;
-            StringBuilder sb = new StringBuilder().append(nums[low]);
-            if (low < high) {
-                sb.append("->");
-                sb.append(nums[high]);
+            int right = i - 1;
+            StringBuilder sb = new StringBuilder().append(nums[left]);
+            if (left < right) {
+                sb.append("->").append(nums[right]);
             }
             res.add(sb.toString());
         }
