4ab238faddeb1ac97e96342d558f0a9e0df0c52f
xionghaiyang <18756618469@163.com>
2025-07-10 11:30:18 +0800

    202. 快乐数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode202/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode202/Solution.java
@@ -1,13 +1,12 @@
 package com.sean.leetcode.LeetCode202;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-23 21:12
- * @Description: https://leetcode.cn/problems/happy-number/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/happy-number
  * 202. 快乐数
  * 编写一个算法来判断一个数 n 是不是快乐数。
  * 「快乐数」 定义为：
@@ -15,6 +14,7 @@ import java.util.Set;
  * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
  * 如果这个过程 结果为 1，那么这个数就是快乐数。
  * 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+ * 1 <= n <= 2^31 - 1
  */
 public class Solution {
 
@@ -27,6 +27,16 @@ public class Solution {
         return n == 1;
     }
 
+    private int getNext(int n) {
+        int res = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n /= 10;
+            res += d * d;
+        }
+        return res;
+    }
+
     public boolean isHappy1(int n) {
         int slow = n;
         int fast = getNext(n);
@@ -35,34 +45,6 @@ public class Solution {
             fast = getNext(getNext(fast));
         }
         return fast == 1;
-    }
-
-    private int getNext(int n) {
-        int res = 0;
-        while (n > 0) {
-            int d = n % 10;
-            n = n / 10;
-            res += d * d;
-        }
-        return res;
-    }
-
-    public boolean isHappy2(int n) {
-//        Set<Integer> set = new HashSet<Integer>() {{
-//            add(4);
-//            add(16);
-//            add(37);
-//            add(58);
-//            add(89);
-//            add(145);
-//            add(42);
-//            add(20);
-//        }};
-        Set<Integer> set = new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
-        while (n != 1 && !set.contains(n)) {
-            n = getNext(n);
-        }
-        return n == 1;
     }
 
 }
