742ab2c360aa391ddb4bd7500c5dd5d44f21e111
xionghaiyang <18756618469@163.com>
2025-07-10 16:40:26 +0800

    57. 插入区间

--- algorithm/src/main/java/com/sean/leetcode/LeetCode57/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode57/Solution.java
@@ -6,37 +6,44 @@ import java.util.List;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-24 20:02
- * @Description: https://leetcode.cn/problems/insert-interval/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/insert-interval
  * 57. 插入区间
  * 给你一个 无重叠的 ，按照区间起始端点排序的区间列表。
  * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
+ * 0 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^5
+ * intervals 根据 starti 按 升序 排列
+ * newInterval.length == 2
+ * 0 <= start <= end <= 10^5
  */
 public class Solution {
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int left = newInterval[0];
-        int right = newInterval[1];
-        boolean flag = false;
         List<int[]> list = new ArrayList<>();
+        int left = newInterval[0], right = newInterval[1];
+        boolean flag = false;
         for (int[] interval : intervals) {
-            if (interval[0] > right) {
+            int start = interval[0], end = interval[1];
+            if (start > right) {
                 if (!flag) {
                     list.add(new int[]{left, right});
                     flag = true;
                 }
                 list.add(interval);
-            } else if (interval[1] < left) {
+            } else if (end < left) {
                 list.add(interval);
             } else {
-                left = Math.min(left, interval[0]);
-                right = Math.max(right, interval[1]);
+                left = Math.min(left, start);
+                right = Math.max(right, end);
             }
         }
         if (!flag) {
             list.add(new int[]{left, right});
         }
-        int[][] res = new int[list.size()][2];
-        for (int i = 0; i < list.size(); i++) {
+        int size = list.size();
+        int[][] res = new int[size][];
+        for (int i = 0; i < size; i++) {
             res[i] = list.get(i);
         }
         return res;
