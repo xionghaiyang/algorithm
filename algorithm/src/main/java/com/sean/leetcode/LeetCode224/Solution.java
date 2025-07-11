72a88cbb5f70aa3d5842df8e4381129cc01cca00
xionghaiyang <18756618469@163.com>
2025-07-11 15:21:35 +0800

    224. 基本计算器

--- algorithm/src/main/java/com/sean/leetcode/LeetCode224/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode224/Solution.java
@@ -6,10 +6,17 @@ import java.util.Deque;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-25 18:39
- * @Description: https://leetcode.cn/problems/basic-calculator/description/
+ * @Description: https://leetcode.cn/problems/basic-calculator
  * 224. 基本计算器
  * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
  * 注意:不允许使用任何将字符串作为数学表达式计算的内置函数，比如 eval() 。
+ * 1 <= s.length <= 3 * 10^5
+ * s 由数字、'+'、'-'、'('、')'、和 ' ' 组成
+ * s 表示一个有效的表达式
+ * '+' 不能用作一元运算(例如， "+1" 和 "+(2 + 3)" 无效)
+ * '-' 可以用作一元运算(即 "-1" 和 "-(2 + 3)" 是有效的)
+ * 输入中不存在两个连续的操作符
+ * 每个数字和运行的计算将适合于一个有符号的 32位 整数
  */
 public class Solution {
 
@@ -37,8 +44,7 @@ public class Solution {
             } else {
                 int num = 0;
                 while (i < n && Character.isDigit(s.charAt(i))) {
-                    num = num * 10 + s.charAt(i) - '0';
-                    i++;
+                    num = num * 10 + s.charAt(i++) - '0';
                 }
                 res += sign * num;
             }
