58dfef4c4ae44264e45597c60d9c759f6e71d059
xionghaiyang <18756618469@163.com>
2025-07-13 06:24:46 +0800

    21. 合并两个有序链表

--- algorithm/src/main/java/com/sean/leetcode/LeetCode21/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode21/Solution.java
@@ -3,7 +3,7 @@ package com.sean.leetcode.LeetCode21;
 /**
  * @Author xionghaiyang
  * @Date 2025-03-23 18:36
- * @Description https://leetcode.cn/problems/merge-two-sorted-lists/
+ * @Description https://leetcode.cn/problems/merge-two-sorted-lists
  * 21. 合并两个有序链表
  * 将两个升序链表合并为一个新的 升序 链表并返回。
  * 新链表是通过拼接给定的两个链表的所有节点组成的。
