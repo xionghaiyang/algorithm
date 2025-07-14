c7222b9be989c536e6c0c66af42e0bab262628a8
xionghaiyang <18756618469@163.com>
2025-07-14 19:40:04 +0800

    82. 删除排序链表中的重复元素 II

--- algorithm/src/main/java/com/sean/leetcode/LeetCode82/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode82/Solution.java
@@ -3,9 +3,13 @@ package com.sean.leetcode.LeetCode82;
 /**
  * @Auther: xionghaiyang
  * @Date: 2024-01-14 15:37
- * @Description: https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/
+ * @Description: https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii
  * 82. 删除排序链表中的重复元素 II
- * 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+ * 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。
+ * 返回 已排序的链表 。
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序 排列
  */
 public class Solution {
 
