e3a6512a114ca0ee2f061051389b554b00394d5f
xionghaiyang <18756618469@163.com>
2025-07-15 16:34:24 +0800

    104. 二叉树的最大深度

--- algorithm/src/main/java/com/sean/leetcode/LeetCode104/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode104/Solution.java
@@ -3,7 +3,7 @@ package com.sean.leetcode.LeetCode104;
 /**
  * @Author xionghaiyang
  * @Date 2025-03-24 21:06
- * @Description https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+ * @Description https://leetcode.cn/problems/maximum-depth-of-binary-tree
  * 104. 二叉树的最大深度
  * 给定一个二叉树 root ，返回其最大深度。
  * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
