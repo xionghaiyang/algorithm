b1db01c1b51ba60833d8ac62622962e1b651d1b0
xionghaiyang <18756618469@163.com>
2025-07-15 20:17:37 +0800

    105. 从前序与中序遍历序列构造二叉树

--- algorithm/src/main/java/com/sean/leetcode/LeetCode105/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode105/Solution.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  * @Auther: xionghaiyang
  * @Date: 2024-02-20 15:32
- * @Description: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ * @Description: https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal
  * 105. 从前序与中序遍历序列构造二叉树
  * 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历，
  * inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
