2bb3619f1e173b297456804fa72dc68be0c35475
xionghaiyang <18756618469@163.com>
2025-07-22 17:22:51 +0800

    22. 括号生成

--- algorithm/src/main/java/com/sean/leetcode/LeetCode22/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode22/Solution.java
@@ -6,40 +6,31 @@ import java.util.List;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-09 20:56
- * @Description: https://leetcode.cn/problems/generate-parentheses/
+ * @Description: https://leetcode.cn/problems/generate-parentheses
  * 22. 括号生成
  * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
  * 1 <= n <= 8
  */
 public class Solution {
 
-    List<String> res = new ArrayList<>();
-    StringBuilder sb = new StringBuilder();
-    int n;
-
     public List<String> generateParenthesis(int n) {
-        if (n <= 0) {
-            return res;
-        }
-        this.n = n;
-        process(0, 0);
+        List<String> res = new ArrayList<>();
+        process(0, 0, n, new char[n * 2], res);
         return res;
     }
 
-    private void process(int left, int right) {
-        if (left == n && right == n) {
-            res.add(sb.toString());
+    private void process(int left, int right, int n, char[] str, List<String> res) {
+        if (right == n) {
+            res.add(String.valueOf(str));
             return;
         }
         if (left < n) {
-            sb.append('(');
-            process(left + 1, right);
-            sb.deleteCharAt(sb.length() - 1);
+            str[left + right] = '(';
+            process(left + 1, right, n, str, res);
         }
         if (left > right) {
-            sb.append(')');
-            process(left, right + 1);
-            sb.deleteCharAt(sb.length() - 1);
+            str[left + right] = ')';
+            process(left, right + 1, n, str, res);
         }
     }
 
