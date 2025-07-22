72ce4275d5a45e00d1f22eb32a3a21d5d7606840
xionghaiyang <18756618469@163.com>
2025-07-22 14:22:29 +0800

    77. 组合

--- algorithm/src/main/java/com/sean/leetcode/LeetCode77/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode77/Solution.java
@@ -6,30 +6,35 @@ import java.util.List;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-18 12:56
- * @Description: https://leetcode.cn/problems/combinations/
+ * @Description: https://leetcode.cn/problems/combinations
  * 77. 组合
  * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
  * 你可以按 任何顺序 返回答案。
+ * 1 <= n <= 20
+ * 1 <= k <= n
  */
 public class Solution {
 
-    List<List<Integer>> res = new ArrayList<>();
+    private List<List<Integer>> res = new ArrayList<>();
+    private List<Integer> temp = new ArrayList<>();
 
     public List<List<Integer>> combine(int n, int k) {
-        process(n, k, new ArrayList<>(), 1);
+        process(n, k, 1);
         return res;
     }
 
-    private void process(int n, int k, List<Integer> list, int index) {
-        if (list.size() == k) {
-            res.add(new ArrayList<>(list));
+    private void process(int n, int k, int i) {
+        if (temp.size() + (n - i + 1) < k) {
             return;
         }
-        for (int i = index; i <= n; i++) {
-            list.add(i);
-            process(n, k, list, i + 1);
-            list.remove(list.size() - 1);
+        if (temp.size() == k) {
+            res.add(new ArrayList<>(temp));
+            return;
         }
+        process(n, k, i + 1);
+        temp.add(i);
+        process(n, k, i + 1);
+        temp.remove(temp.size() - 1);
     }
 
 }
