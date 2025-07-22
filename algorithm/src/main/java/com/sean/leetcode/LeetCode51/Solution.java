a9bc3b550dc7836d6baf4f9a0d8160a7d9b4f383
xionghaiyang <18756618469@163.com>
2025-07-22 16:39:32 +0800

    51. N 皇后

--- algorithm/src/main/java/com/sean/leetcode/LeetCode51/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode51/Solution.java
@@ -7,68 +7,48 @@ import java.util.List;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-08-29 19:26
- * @Description: https://leetcode.cn/problems/n-queens/
+ * @Description: https://leetcode.cn/problems/n-queens
  * 51. N 皇后
  * 按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
  * n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
  * 给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
  * 每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ * 1 <= n <= 9
  */
 public class Solution {
 
-    private int n;
-    private char[][] chess;
-    private List<List<String>> res = new ArrayList<>();
-
     public List<List<String>> solveNQueens(int n) {
-        this.n = n;
-        chess = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(chess[i], '.');
-        }
-        process(0);
+        List<List<String>> res = new ArrayList<>();
+        int[] queens = new int[n];
+        boolean[] col = new boolean[n];
+        boolean[] diag1 = new boolean[n * 2 - 1];
+        boolean[] diag2 = new boolean[n * 2 - 1];
+        dfs(0, queens, col, diag1, diag2, res);
         return res;
     }
 
-    private void process(int row) {
-        if (row == n) {
-            res.add(build());
+    private void dfs(int r, int[] queens, boolean[] col, boolean[] diag1, boolean[] diag2, List<List<String>> res) {
+        int n = col.length;
+        if (r == n) {
+            List<String> board = new ArrayList<>(n);
+            for (int c : queens) {
+                char[] row = new char[n];
+                Arrays.fill(row, '.');
+                row[c] = 'Q';
+                board.add(String.valueOf(row));
+            }
+            res.add(board);
             return;
         }
-        for (int col = 0; col < n; col++) {
-            if (valid(row, col)) {
-                chess[row][col] = 'Q';
-                process(row + 1);
-                chess[row][col] = '.';
+        for (int c = 0; c < n; c++) {
+            int rc = r - c + n - 1;
+            if (!col[c] && !diag1[r + c] && !diag2[rc]) {
+                queens[r] = c;
+                col[c] = diag1[r + c] = diag2[rc] = true;
+                dfs(r + 1, queens, col, diag1, diag2, res);
+                col[c] = diag1[r + c] = diag2[rc] = false;
             }
         }
-    }
-
-    private boolean valid(int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (chess[i][col] == 'Q') {
-                return false;
-            }
-        }
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (chess[i][j] == 'Q') {
-                return false;
-            }
-        }
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (chess[i][j] == 'Q') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private List<String> build() {
-        List<String> res = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            res.add(String.valueOf(chess[i]));
-        }
-        return res;
     }
 
 }
