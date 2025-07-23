edd328aed3b2b6ca40b0f37aae2f241e4d774355
xionghaiyang <18756618469@163.com>
2025-07-23 11:06:46 +0800

    53. 最大子数组和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode53/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode53/Solution.java
@@ -3,7 +3,7 @@ package com.sean.leetcode.LeetCode53;
 /**
  * @Author xionghaiyang
  * @Date 2024-05-07 18:24
- * @Description https://leetcode.cn/problems/maximum-subarray/
+ * @Description https://leetcode.cn/problems/maximum-subarray
  * 53. 最大子数组和
  * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
  * 子数组是数组中的一个连续部分。
@@ -13,8 +13,7 @@ package com.sean.leetcode.LeetCode53;
 public class Solution {
 
     public int maxSubArray(int[] nums) {
-        int res = nums[0];
-        int sum = 0;
+        int res = nums[0], sum = 0;
         for (int num : nums) {
             if (sum > 0) {
                 sum += num;
