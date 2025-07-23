1e65fbfe59ed7be2a73fef19cff6b46e76f04860
xionghaiyang <18756618469@163.com>
2025-07-23 13:30:15 +0800

    918. 环形子数组的最大和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode918/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode918/Solution.java
@@ -1,39 +1,40 @@
 package com.sean.leetcode.LeetCode918;
 
-import java.util.LinkedList;
-
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-14 21:18
- * @Description: https://leetcode.cn/problems/maximum-sum-circular-subarray/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/maximum-sum-circular-subarray
  * 918. 环形子数组的最大和
  * 给定一个长度为 n 的环形整数数组 nums ，返回 nums 的非空 子数组 的最大可能和 。
  * 环形数组 意味着数组的末端将会与开头相连呈环状。
  * 形式上， nums[i] 的下一个元素是 nums[(i + 1) % n] ， nums[i] 的前一个元素是 nums[(i - 1 + n) % n] 。
- * 子数组 最多只能包含固定缓冲区 nums 中的每个元素一次。形式上，对于子数组 nums[i], nums[i + 1], ..., nums[j] ，不存在 i <= k1, k2 <= j 其中 k1 % n == k2 % n 。
+ * 子数组 最多只能包含固定缓冲区 nums 中的每个元素一次。
+ * 形式上，对于子数组 nums[i], nums[i + 1], ..., nums[j] ，不存在 i <= k1, k2 <= j 其中 k1 % n == k2 % n 。
+ * n == nums.length
+ * 1 <= n <= 3 * 10^4
+ * -3 * 10^4 <= nums[i] <= 3 * 10^4​​​​​​​
  */
 public class Solution {
 
     public int maxSubarraySumCircular(int[] nums) {
         int n = nums.length;
-        int[] preSum = new int[2 * n + 1];
-        for (int i = 0; i < 2 * n; i++) {
-            preSum[i + 1] = preSum[i] + nums[i % n];
+        int preMax = nums[0], maxRes = nums[0];
+        int preMin = nums[0], minRes = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < n; i++) {
+            //以nums[i]结尾的最大子数组和
+            preMax = Math.max(preMax + nums[i], nums[i]);
+            maxRes = Math.max(maxRes, preMax);
+            //以nums[i]结尾的最小子数组和
+            preMin = Math.min(preMin + nums[i], nums[i]);
+            minRes = Math.min(minRes, preMin);
+            sum += nums[i];
         }
-        int res = nums[0];
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.offer(0);
-        for (int j = 1; j <= 2 * n; j++) {
-            if (linkedList.peekFirst() < j - n) {
-                linkedList.pollFirst();
-            }
-            res = Math.max(res, preSum[j] - preSum[linkedList.peekFirst()]);
-            while (!linkedList.isEmpty() && preSum[j] <= preSum[linkedList.peekLast()]) {
-                linkedList.pollLast();
-            }
-            linkedList.offerLast(j);
+        if (maxRes < 0) {
+            return maxRes;
+        } else {
+            return Math.max(maxRes, sum - minRes);
         }
-        return res;
     }
 
 }
