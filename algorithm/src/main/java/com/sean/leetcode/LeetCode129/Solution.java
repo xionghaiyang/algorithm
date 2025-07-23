549c01895f2737315002f6317ca9d7dc1b652a6b
xionghaiyang <18756618469@163.com>
2025-07-23 20:37:05 +0800

    129. 求根节点到叶节点数字之和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode129/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode129/Solution.java
@@ -3,13 +3,16 @@ package com.sean.leetcode.LeetCode129;
 /**
  * @Auther: xionghaiyang
  * @Date: 2024-01-12 15:26
- * @Description: https://leetcode.cn/problems/sum-root-to-leaf-numbers/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/sum-root-to-leaf-numbers
  * 129. 求根节点到叶节点数字之和
  * 给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
  * 每条从根节点到叶节点的路径都代表一个数字：
  * 例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
  * 计算从根节点到叶节点生成的 所有数字之和 。
  * 叶节点 是指没有子节点的节点。
+ * 树中节点的数目在范围 [1, 1000] 内
+ * 0 <= Node.val <= 9
+ * 树的深度不超过 10
  */
 public class Solution {
 
@@ -36,11 +39,11 @@ public class Solution {
         return dfs(root, 0);
     }
 
-    private int dfs(TreeNode root, int prevSum) {
+    private int dfs(TreeNode root, int preSum) {
         if (root == null) {
             return 0;
         }
-        int sum = prevSum * 10 + root.val;
+        int sum = preSum * 10 + root.val;
         if (root.left == null && root.right == null) {
             return sum;
         } else {
