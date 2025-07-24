04a57f436a49f4584007f1202d21a7737ce60e44
xionghaiyang <18756618469@163.com>
2025-07-24 19:05:53 +0800

    173. 二叉搜索树迭代器

--- algorithm/src/main/java/com/sean/leetcode/LeetCode173/BSTIterator.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode173/BSTIterator.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * @Auther: xionghaiyang
  * @Date: 2024-04-12 15:05
- * @Description: https://leetcode.cn/problems/binary-search-tree-iterator/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/binary-search-tree-iterator
  * 173. 二叉搜索树迭代器
  * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
  * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。
@@ -16,6 +16,9 @@ import java.util.List;
  * int next()将指针向右移动，然后返回指针处的数字。
  * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
  * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+ * 树中节点的数目在范围 [1, 10^5] 内
+ * 0 <= Node.val <= 10^6
+ * 最多调用 10^5 次 hasNext 和 next 操作
  */
 public class BSTIterator {
 
@@ -39,20 +42,12 @@ public class BSTIterator {
     }
 
     private int index;
-    private List<Integer> arr;
+    private List<Integer> list;
 
     public BSTIterator(TreeNode root) {
         index = 0;
-        arr = new ArrayList<>();
+        list = new ArrayList<>();
         dfs(root);
-    }
-
-    public int next() {
-        return arr.get(index++);
-    }
-
-    public boolean hasNext() {
-        return index < arr.size();
     }
 
     private void dfs(TreeNode root) {
@@ -60,8 +55,16 @@ public class BSTIterator {
             return;
         }
         dfs(root.left);
-        arr.add(root.val);
+        list.add(root.val);
         dfs(root.right);
+    }
+
+    public int next() {
+        return list.get(index++);
+    }
+
+    public boolean hasNext() {
+        return index < list.size();
     }
 
 }
