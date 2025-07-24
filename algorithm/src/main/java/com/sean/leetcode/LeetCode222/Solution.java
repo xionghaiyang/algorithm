fc816d258f9e04d648258c26cd390ba40401c4e1
xionghaiyang <18756618469@163.com>
2025-07-24 20:03:08 +0800

    222. 完全二叉树的节点个数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode222/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode222/Solution.java
@@ -3,12 +3,15 @@ package com.sean.leetcode.LeetCode222;
 /**
  * @Auther: xionghaiyang
  * @Date: 2024-04-13 21:17
- * @Description: https://leetcode.cn/problems/count-complete-tree-nodes/?envType=study-plan-v2&envId=top-interview-150
+ * @Description: https://leetcode.cn/problems/count-complete-tree-nodes
  * 222. 完全二叉树的节点个数
  * 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
  * 完全二叉树 的定义如下：
  * 在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。
  * 若最底层为第 h 层，则该层包含 1~ 2^h 个节点。
+ * 树中节点的数目范围是[0, 5 * 10^4]
+ * 0 <= Node.val <= 5 * 10^4
+ * 题目数据保证输入的树是 完全二叉树
  */
 public class Solution {
 
@@ -32,39 +35,21 @@ public class Solution {
     }
 
     public int countNodes(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int level = 0;
+        int leftHight = 0, rightHight = 0;
         TreeNode node = root;
-        while (node.left != null) {
-            level++;
+        while (node != null) {
             node = node.left;
+            leftHight++;
         }
-        int low = 1 << level, high = (1 << (level + 1)) - 1;
-        while (low < high) {
-            int mid = (high - low + 1) / 2 + low;
-            if (exists(root, level, mid)) {
-                low = mid;
-            } else {
-                high = mid - 1;
-            }
+        node = root;
+        while (node != null) {
+            node = node.right;
+            rightHight++;
         }
-        return low;
-    }
-
-    private boolean exists(TreeNode root, int level, int k) {
-        int bits = 1 << (level - 1);
-        TreeNode node = root;
-        while (node != null && bits > 0) {
-            if ((bits & k) == 0) {
-                node = node.left;
-            } else {
-                node = node.right;
-            }
-            bits >>= 1;
+        if (leftHight == rightHight) {
+            return (int) Math.pow(2, leftHight) - 1;
         }
-        return node != null;
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
 }
