f08aaf2f4752509b404b55c885920fefa257e288
xionghaiyang <18756618469@163.com>
2025-07-26 15:52:59 +0800

    198. 打家劫舍

--- algorithm/src/main/java/com/sean/leetcode/LeetCode198/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode198/Solution.java
@@ -17,20 +17,6 @@ public class Solution {
 
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) {
-            return nums[0];
-        }
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
-        }
-        return dp[n - 1];
-    }
-
-    public int rob1(int[] nums) {
-        int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
         return process(nums, dp, 0);
@@ -49,6 +35,15 @@ public class Solution {
         res = Math.max(res, process(nums, dp, i + 1));
         dp[i] = res;
         return res;
+    }
+
+    public int rob1(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 2];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+        return dp[0];
     }
 
 }
