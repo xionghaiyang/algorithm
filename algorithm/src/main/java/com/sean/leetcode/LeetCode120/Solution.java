613941fc2aebc432b53408e34f2ecbf066a66bb0
xionghaiyang <18756618469@163.com>
2025-07-26 19:57:01 +0800

    120. 三角形最小路径和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode120/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode120/Solution.java
@@ -1,47 +1,36 @@
 package com.sean.leetcode.LeetCode120;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-28 17:03
- * @Description: https://leetcode.cn/problems/triangle/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/triangle
  * 120. 三角形最小路径和
  * 给定一个三角形 triangle ，找出自顶向下的最小路径和。
  * 每一步只能移动到下一行中相邻的结点上。
  * 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
  * 也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+ * 1 <= triangle.length <= 200
+ * triangle[0].length == 1
+ * triangle[i].length == triangle[i - 1].length + 1
+ * -10^4 <= triangle[i][j] <= 10^4
  */
 public class Solution {
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        if (triangle == null || triangle.size() == 0) {
-            return 0;
-        }
         int n = triangle.size();
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        int[] dp = new int[n];
+        //第n-1层
+        for (int j = 0; j < n; j++) {
+            dp[j] = triangle.get(n - 1).get(j);
         }
-        return process(triangle, 0, 0, dp);
-    }
-
-    private int process(List<List<Integer>> triangle, int i, int j, int[][] dp) {
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j + 1], dp[j]) + triangle.get(i).get(j);
+            }
         }
-        int n = triangle.size();
-        if (i == n - 1) {
-            dp[i][j] = triangle.get(n - 1).get(j);
-            return triangle.get(n - 1).get(j);
-        }
-        int res = triangle.get(i).get(j) + process(triangle, i + 1, j, dp);
-        if (j + 1 < triangle.get(i + 1).size()) {
-            res = Math.min(res, triangle.get(i).get(j) + process(triangle, i + 1, j + 1, dp));
-        }
-        dp[i][j] = res;
-        return res;
+        return dp[0];
     }
 
 }
