453c63ce060b85d38413fa920e3541406f42a8f5
xionghaiyang <18756618469@163.com>
2025-07-26 17:23:41 +0800

    139. 单词拆分

--- algorithm/src/main/java/com/sean/leetcode/LeetCode139/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode139/Solution.java
@@ -33,7 +33,7 @@ public class Solution {
         return process(n, maxLength, s, set, dp) == 1;
     }
 
-    private int process(int i, int maxLen, String s, Set<String> set, int[] dp) {
+    private int process(int i, int maxLength, String s, Set<String> set, int[] dp) {
         if (dp[i] != -1) {
             return dp[i];
         }
@@ -41,14 +41,34 @@ public class Solution {
             dp[i] = 1;
             return dp[i];
         }
-        for (int j = i - 1; j >= Math.max(i - maxLen, 0); j--) {
-            if (set.contains(s.substring(j, i)) && process(j, maxLen, s, set, dp) == 1) {
+        for (int j = i - 1; j >= Math.max(i - maxLength, 0); j--) {
+            if (set.contains(s.substring(j, i)) && process(j, maxLength, s, set, dp) == 1) {
                 dp[i] = 1;
                 return dp[i];
             }
         }
         dp[i] = 0;
         return dp[i];
+    }
+
+    public boolean wordBreak1(String s, List<String> wordDict) {
+        int maxLength = 0;
+        for (String word : wordDict) {
+            maxLength = Math.max(maxLength, word.length());
+        }
+        Set<String> set = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i - 1; j >= Math.max(i - maxLength, 0); j--) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 
 }
