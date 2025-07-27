a3893daa1f3644f7dc7033a5e50005d2964b5a32
xionghaiyang <18756618469@163.com>
2025-07-27 22:00:06 +0800

    307. 区域和检索 - 数组可修改

--- algorithm/src/main/java/com/sean/leetcode/LeetCode307/NumArray.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode307/NumArray.java
@@ -3,7 +3,7 @@ package com.sean.leetcode.LeetCode307;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-11-13 16:30
- * @Description: https://leetcode.cn/problems/range-sum-query-mutable/description/
+ * @Description: https://leetcode.cn/problems/range-sum-query-mutable
  * 307. 区域和检索 - 数组可修改
  * 给你一个数组 nums ，请你完成两类查询。
  * 其中一类查询要求 更新 数组 nums 下标对应的值
@@ -13,50 +13,50 @@ package com.sean.leetcode.LeetCode307;
  * void update(int index, int val) 将 nums[index] 的值 更新 为 val
  * int sumRange(int left, int right) 返回数组 nums 中索引 left
  * ，和索引 right 之间（ 包含 ）的nums元素的 和 （即，nums[left] + nums[left + 1], ..., nums[right]）
+ * 1 <= nums.length <= 3 * 10^4
+ * -100 <= nums[i] <= 100
+ * 0 <= index < nums.length
+ * -100 <= val <= 100
+ * 0 <= left <= right < nums.length
+ * 调用 update 和 sumRange 方法次数不大于 3 * 10^4
  */
 public class NumArray {
 
-    private int[] sum;//sum[i]表示第i个块的元素和
-    private int size;//块的大小
     private int[] nums;
+    private int[] tree;
 
     public NumArray(int[] nums) {
-        this.nums = nums;
         int n = nums.length;
-        size = (int) Math.sqrt(n);
-        sum = new int[(n + size - 1) / size];//  n/size向上取整
-        for (int i = 0; i < n; i++) {
-            sum[i / size] += nums[i];
+        this.nums = nums;
+        tree = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            tree[i] += nums[i - 1];
+            int next = i + (i & -i);
+            if (next <= n) {
+                tree[next] += tree[i];
+            }
         }
     }
 
     public void update(int index, int val) {
-        sum[index / size] += val - nums[index];
+        int delta = val - nums[index];
         nums[index] = val;
+        for (int i = index + 1; i < tree.length; i += i & -i) {
+            tree[i] += delta;
+        }
     }
 
     public int sumRange(int left, int right) {
-        int b1 = left / size, i1 = left % size, b2 = right / size, i2 = right % size;
-        if (b1 == b2) {//区间[left,right]在同一块中
-            int sum = 0;
-            for (int j = i1; j <= i2; j++) {
-                sum += nums[b1 * size + j];
-            }
-            return sum;
+        return prefixSum(right + 1) - prefixSum(left);
+    }
+
+    private int prefixSum(int i) {
+        int res = 0;
+        while (i > 0) {
+            res += tree[i];
+            i &= i - 1;//i-=i & -i
         }
-        int sum1 = 0;
-        for (int j = i1; j < size; j++) {
-            sum1 += nums[b1 * size + j];
-        }
-        int sum2 = 0;
-        for (int j = 0; j <= i2; j++) {
-            sum2 += nums[b2 * size + j];
-        }
-        int sum3 = 0;
-        for (int j = b1 + 1; j < b2; j++) {
-            sum3 += sum[j];
-        }
-        return sum1 + sum2 + sum3;
+        return res;
     }
 
 }
