3701e1801e5b79c68679a1f6a6ae23527cae3530
xionghaiyang <18756618469@163.com>
2025-07-28 14:39:55 +0800

    97. 交错字符串

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode97/Solution.java
@@ -0,0 +1,63 @@
+package com.sean.leetcode.LeetCode97;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-07-28 13:44
+ * @Description https://leetcode.cn/problems/interleaving-string
+ * 97. 交错字符串
+ * 给定三个字符串 s1、s2、s3，请你帮忙验证 s3 是否是由 s1 和 s2 交错 组成的。
+ * 两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+ * s = s1 + s2 + ... + sn
+ * t = t1 + t2 + ... + tm
+ * |n - m| <= 1
+ * 交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
+ * 注意：a + b 意味着字符串 a 和 b 连接。
+ * 0 <= s1.length, s2.length <= 100
+ * 0 <= s3.length <= 200
+ * s1、s2、和 s3 都由小写英文字母组成
+ */
+public class Solution {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length();
+        if (m + n != s3.length()) {
+            return false;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(p));
+                }
+                if (j > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public boolean isInterleave1(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length();
+        if (m + n != s3.length()) {
+            return false;
+        }
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(p);
+                }
+                if (j > 0) {
+                    dp[j] = dp[j] || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+        return dp[n];
+    }
+
+}
