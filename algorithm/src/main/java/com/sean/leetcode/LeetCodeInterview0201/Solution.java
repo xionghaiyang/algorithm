596e7e5c298dcde8dad67d9f41b24b273ce70f16
xionghaiyang <18756618469@163.com>
2025-07-29 12:55:03 +0800

    面试题 02.01. 移除重复节点

--- algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview0201/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview0201/Solution.java
@@ -6,9 +6,11 @@ import java.util.Set;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-10-22 08:41
- * @Description: https://leetcode.cn/problems/remove-duplicate-node-lcci/
+ * @Description: https://leetcode.cn/problems/remove-duplicate-node-lcci
  * 面试题 02.01. 移除重复节点
- * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+ * 编写代码，移除未排序链表中的重复节点。
+ * 保留最开始出现的节点。
+ * 链表长度在[0, 20000]范围内。
  */
 public class Solution {
 
@@ -23,7 +25,7 @@ public class Solution {
 
     public ListNode removeDuplicateNodes(ListNode head) {
         if (head == null) {
-            return null;
+            return head;
         }
         Set<Integer> set = new HashSet<>();
         set.add(head.val);
@@ -35,6 +37,22 @@ public class Solution {
                 set.add(cur.next.val);
                 cur = cur.next;
             }
+        }
+        return head;
+    }
+
+    public ListNode removeDuplicateNodes1(ListNode head) {
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode cur = pre;
+            while (cur.next != null) {
+                if (cur.next.val == pre.val) {
+                    cur.next = cur.next.next;
+                } else {
+                    cur = cur.next;
+                }
+            }
+            pre = pre.next;
         }
         return head;
     }
