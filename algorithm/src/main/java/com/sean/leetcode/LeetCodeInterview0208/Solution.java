7a360a8b4294a35fde4974e997f5c84ce5c1a1b8
xionghaiyang <18756618469@163.com>
2025-08-01 12:16:42 +0800

    面试题 02.08. 环路检测

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview0208/Solution.java
@@ -0,0 +1,51 @@
+package com.sean.leetcode.LeetCodeInterview0208;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-08-01 11:13
+ * @Description https://leetcode.cn/problems/linked-list-cycle-lcci
+ * 面试题 02.08. 环路检测
+ * 给定一个链表，如果它是有环链表，实现一个算法返回环路的开头节点。
+ * 若环不存在，请返回 null。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。
+ * 注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ */
+public class Solution {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if (fast == slow) {
+                fast = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+}
