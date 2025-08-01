8fc4f7bc2324aa48a0ddec2e01e1c34dec7eef26
xionghaiyang <18756618469@163.com>
2025-08-01 10:48:13 +0800

    3622. 判断整除性

--- algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview0207/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview0207/Solution.java
@@ -5,7 +5,8 @@ package com.sean.leetcode.LeetCodeInterview0207;
  * @Date 2025-08-01 10:35
  * @Description https://leetcode.cn/problems/intersection-of-two-linked-lists-lcci
  * 面试题 02.07. 链表相交
- * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。
+ * 如果两个链表没有交点，返回 null 。
  * 图示两个链表在节点 c1 开始相交：
  * 题目数据 保证 整个链式结构中不存在环。
  * 注意，函数返回结果后，链表必须 保持其原始结构 。
