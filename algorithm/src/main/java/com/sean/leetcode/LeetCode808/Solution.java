ef288a172157eb6dfd4cb492ccc8b64415095fb3
xionghaiyang <18756618469@163.com>
2025-08-08 09:31:54 +0800

    808. 分汤

--- algorithm/src/main/java/com/sean/leetcode/LeetCode808/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode808/Solution.java
@@ -3,7 +3,7 @@ package com.sean.leetcode.LeetCode808;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-11-21 09:23
- * @Description: https://leetcode.cn/problems/soup-servings/
+ * @Description: https://leetcode.cn/problems/soup-servings
  * 808. 分汤
  * 有 A 和 B 两种类型 的汤。一开始每种类型的汤有 n 毫升。有四种分配操作：
  * 提供 100ml 的 汤A 和 0ml 的 汤B 。
@@ -16,33 +16,32 @@ package com.sean.leetcode.LeetCode808;
  * 当两种类型的汤都分配完时，停止操作。
  * 注意 不存在先分配 100 ml 汤B 的操作。
  * 需要返回的值： 汤A 先分配完的概率 +  汤A和汤B 同时分配完的概率 / 2。
- * 返回值在正确答案 10-5 的范围内将被认为是正确的。
+ * 返回值在正确答案 10^-5 的范围内将被认为是正确的。
+ * 0 <= n <= 10^9
  */
 public class Solution {
-
-    private double[][] memo;
 
     public double soupServings(int n) {
         n = (int) Math.ceil((double) n / 25);
         if (n >= 179) {
             return 1.0;
         }
-        memo = new double[n + 1][n + 1];
-        return dfs(n, n);
+        double[][] memo = new double[n + 1][n + 1];
+        return dfs(n, n, memo);
     }
 
-    public double dfs(int a, int b) {
-        if (a <= 0 && b <= 0) {
+    private double dfs(int i, int j, double[][] memo) {
+        if (i <= 0 && j <= 0) {
             return 0.5;
-        } else if (a <= 0) {
-            return 1;
-        } else if (b <= 0) {
+        } else if (i <= 0) {
+            return 1.0;
+        } else if (j <= 0) {
             return 0;
         }
-        if (memo[a][b] == 0) {
-            memo[a][b] = 0.25 * (dfs(a - 4, b) + dfs(a - 3, b - 1) + dfs(a - 2, b - 2) + dfs(a - 1, b - 3));
+        if (memo[i][j] == 0) {
+            memo[i][j] = 0.25 * (dfs(i - 4, j, memo) + dfs(i - 3, j - 1, memo) + dfs(i - 2, j - 2, memo) + dfs(i - 1, j - 3, memo));
         }
-        return memo[a][b];
+        return memo[i][j];
     }
 
 }
