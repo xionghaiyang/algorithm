133a7557d843814b33dd921f4862444a25e9819a
xionghaiyang <18756618469@163.com>
2025-08-14 10:44:21 +0800

    463. 岛屿的周长

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode463/Solution.java
@@ -0,0 +1,43 @@
+package com.sean.leetcode.LeetCode463;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-08-14 10:38
+ * @Description https://leetcode.cn/problems/island-perimeter
+ * 463. 岛屿的周长
+ * 给定一个 row x col 的二维网格地图 grid ，其中：grid[i][j] = 1 表示陆地， grid[i][j] = 0 表示水域。
+ * 网格中的格子 水平和垂直 方向相连（对角线方向不相连）。
+ * 整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+ * 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。
+ * 格子是边长为 1 的正方形。
+ * 网格为长方形，且宽度和高度均不超过 100 。
+ * 计算这个岛屿的周长。
+ * row == grid.length
+ * col == grid[i].length
+ * 1 <= row, col <= 100
+ * grid[i][j] 为 0 或 1
+ */
+public class Solution {
+
+    public int islandPerimeter(int[][] grid) {
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int m = grid.length, n = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    int cnt = 0;
+                    for (int[] dir : dirs) {
+                        int x = i + dir[0], y = j + dir[1];
+                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0) {
+                            cnt++;
+                        }
+                    }
+                    res += cnt;
+                }
+            }
+        }
+        return res;
+    }
+
+}
