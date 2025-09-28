455296c06b4ab973dc962a9db03b80e9278fe16c
xionghaiyang <18756618469@163.com>
2025-09-28 18:13:50 +0800

    566. 重塑矩阵

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode566/Solution.java
@@ -0,0 +1,32 @@
+package com.sean.leetcode.LeetCode566;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-09-28 18:05
+ * @Description https://leetcode.cn/problems/reshape-the-matrix
+ * 566. 重塑矩阵
+ * 在 MATLAB 中，有一个非常有用的函数 reshape ，它可以将一个 m x n 矩阵重塑为另一个大小不同（r x c）的新矩阵，但保留其原始数据。
+ * 给你一个由二维数组 mat 表示的 m x n 矩阵，以及两个正整数 r 和 c ，分别表示想要的重构的矩阵的行数和列数。
+ * 重构后的矩阵需要将原始矩阵的所有元素以相同的 行遍历顺序 填充。
+ * 如果具有给定参数的 reshape 操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 100
+ * -1000 <= mat[i][j] <= 1000
+ * 1 <= r, c <= 300
+ */
+public class Solution {
+
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length, n = mat[0].length;
+        if (m * n != r * c) {
+            return mat;
+        }
+        int[][] res = new int[r][c];
+        for (int i = 0; i < m * n; i++) {
+            res[i / c][i % c] = mat[i / n][i % n];
+        }
+        return res;
+    }
+
+}
