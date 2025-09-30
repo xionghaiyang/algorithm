a2f4749bd0e3b861b3f48ab343f7b047fe6427fe
xionghaiyang <18756618469@163.com>
2025-09-30 12:53:09 +0800

    2221. 数组的三角和

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode2221/Solution.java
@@ -0,0 +1,31 @@
+package com.sean.leetcode.LeetCode2221;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-09-30 12:23
+ * @Description https://leetcode.cn/problems/find-triangular-sum-of-an-array
+ * 2221. 数组的三角和
+ * 给你一个下标从 0 开始的整数数组 nums ，其中 nums[i] 是 0 到 9 之间（两者都包含）的一个数字。
+ * nums 的 三角和 是执行以下操作以后最后剩下元素的值：
+ * nums 初始包含 n 个元素。
+ * 如果 n == 1 ，终止 操作。
+ * 否则，创建 一个新的下标从 0 开始的长度为 n - 1 的整数数组 newNums 。
+ * 对于满足 0 <= i < n - 1 的下标 i ，newNums[i] 赋值 为 (nums[i] + nums[i+1]) % 10 ，% 表示取余运算。
+ * 将 newNums 替换 数组 nums 。
+ * 从步骤 1 开始 重复 整个过程。
+ * 请你返回 nums 的三角和。
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] <= 9
+ */
+public class Solution {
+
+    public int triangularSum(int[] nums) {
+        for (int n = nums.length - 1; n >= 1; n--) {
+            for (int i = 0; i < n; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+        }
+        return nums[0];
+    }
+
+}
