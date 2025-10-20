5c9362a38495b582dd069d953a210ed577e78a68
xionghaiyang <18756618469@163.com>
2025-10-20 14:00:19 +0800

    832. 翻转图像

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode832/Solution.java
@@ -0,0 +1,31 @@
+package com.sean.leetcode.LeetCode832;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-10-20 13:44
+ * @Description https://leetcode.cn/problems/flipping-an-image
+ * 832. 翻转图像
+ * 给定一个 n x n 的二进制矩阵 image ，先 水平 翻转图像，然后 反转 图像并返回 结果 。
+ * 水平翻转图片就是将图片的每一行都进行翻转，即逆序。
+ * 例如，水平翻转 [1,1,0] 的结果是 [0,1,1]。
+ * 反转图片的意思是图片中的 0 全部被 1 替换， 1 全部被 0 替换。
+ * 例如，反转 [0,1,1] 的结果是 [1,0,0]。
+ * n == image.length
+ * n == image[i].length
+ * 1 <= n <= 20
+ * images[i][j] == 0 或 1.
+ */
+public class Solution {
+
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        int[][] res = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = image[i][n - 1 - j] == 0 ? 1 : 0;
+            }
+        }
+        return res;
+    }
+
+}
