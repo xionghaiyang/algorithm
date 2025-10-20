42ba88be149e44284bfed09da179be43720fca28
xionghaiyang <18756618469@163.com>
2025-10-20 19:24:29 +0800

    面试题 04.02. 最小高度树

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview0402/Solution.java
@@ -0,0 +1,46 @@
+package com.sean.leetcode.LeetCodeInterview0402;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2025-10-20 18:58
+ * @Description https://leetcode.cn/problems/minimum-height-tree-lcci
+ * 面试题 04.02. 最小高度树
+ * 给定一个有序整数数组，元素各不相同且按升序排列，编写一个算法，创建一棵高度最小的二叉搜索树。
+ */
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + ((right - left) >> 1);
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = build(nums, left, mid - 1);
+        root.right = build(nums, mid + 1, right);
+        return root;
+    }
+
+}
