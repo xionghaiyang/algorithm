7cd11850d54b2f9db6968d1f31bb4be700810997
xionghaiyang <18756618469@163.com>
2025-11-25 10:59:42 +0800

    1015. 可被 K 整除的最小整数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1015/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1015/Solution.java
@@ -1,33 +1,28 @@
 package com.sean.leetcode.LeetCode1015;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-05-10 08:11
- * @Description: https://leetcode.cn/problems/smallest-integer-divisible-by-k/
+ * @Description: https://leetcode.cn/problems/smallest-integer-divisible-by-k
  * 1015. 可被 K 整除的最小整数
  * 给定正整数 k ，你需要找出可以被 k 整除的、仅包含数字 1 的最 小 正整数 n 的长度。
- * 返回 n 的长度。如果不存在这样的 n ，就返回-1。
+ * 返回 n 的长度。
+ * 如果不存在这样的 n ，就返回-1。
  * 注意： n 不符合 64 位带符号整数。
+ * 1 <= k <= 10^5
  */
 public class Solution {
 
     public int smallestRepunitDivByK(int k) {
-        int rest = 1 % k;
-        int res = 1;
-        Set<Integer> set = new HashSet<>();
-        set.add(rest);
-        while (rest != 0) {
-            rest = (rest * 10 + 1) % k;
-            res++;
-            if (set.contains(rest)) {
-                return -1;
-            }
-            set.add(rest);
+        if (k % 2 == 0 || k % 5 == 0) {
+            return -1;
         }
-        return res;
+        for (int i = 1, x = 1 % k; ; i++) {
+            if (x == 0) {
+                return i;
+            }
+            x = (x * 10 + 1) % k;
+        }
     }
 
 }
