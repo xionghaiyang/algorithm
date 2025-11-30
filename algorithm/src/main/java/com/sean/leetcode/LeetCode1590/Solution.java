e7ace6795f100426ecd61391aa2cda868c8eb4ba
xionghaiyang <18756618469@163.com>
2025-11-30 19:54:19 +0800

    1590. 使数组和能被 P 整除

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1590/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1590/Solution.java
@@ -6,12 +6,15 @@ import java.util.Map;
 /**
  * @Author xionghaiyang
  * @Date 2024-05-13 20:57
- * @Description https://leetcode.cn/problems/make-sum-divisible-by-p/
+ * @Description https://leetcode.cn/problems/make-sum-divisible-by-p
  * 1590. 使数组和能被 P 整除
  * 给你一个正整数数组 nums，请你移除 最短 子数组（可以为 空），使得剩余元素的 和 能被 p 整除。
  * 不允许 将整个数组都移除。
  * 请你返回你需要移除的最短子数组的长度，如果无法满足题目要求，返回 -1 。
  * 子数组 定义为原数组中连续的一组元素。
+ * 1 <= nums.length <= 10^5
+ * 1 <= nums[i] <= 10^9
+ * 1 <= p <= 10^9
  */
 public class Solution {
 
@@ -25,16 +28,14 @@ public class Solution {
         }
         int q = sum;
         sum = 0;
+        int n = nums.length, res = n;
         Map<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
-        int res = n;
         for (int i = 0; i < n; i++) {
             map.put(sum, i);
             sum = (sum + nums[i]) % p;
             int key = (sum - q + p) % p;
             if (map.containsKey(key)) {
-                int prev = map.get(key);
-                res = Math.min(res, i - prev + 1);
+                res = Math.min(res, i - map.get(key) + 1);
             }
         }
         return res == n ? -1 : res;
