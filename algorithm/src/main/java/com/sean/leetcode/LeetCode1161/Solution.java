b312abfdf96f9f46fb11c828626e9f69d42ff534
xionghaiyang <18756618469@163.com>
2026-01-06 10:04:10 +0800

    1161. 最大层内元素和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1161/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1161/Solution.java
@@ -6,9 +6,13 @@ import java.util.Queue;
 /**
  * @Author xionghaiyang
  * @Date 2022-07-31 08:36
- * @Description https://leetcode.cn/problems/maximum-level-sum-of-a-binary-tree/
- * 给你一个二叉树的根节点 root。设根节点位于二叉树的第 1 层，而根节点的子节点位于第 2 层，依此类推。
+ * @Description https://leetcode.cn/problems/maximum-level-sum-of-a-binary-tree
+ * 1161. 最大层内元素和
+ * 给你一个二叉树的根节点 root。
+ * 设根节点位于二叉树的第 1 层，而根节点的子节点位于第 2 层，依此类推。
  * 请返回层内元素之和 最大 的那几层（可能只有一层）的层号，并返回其中 最小 的那个。
+ * 树中的节点数在 [1, 10^4]范围内
+ * -10^5 <= Node.val <= 10^5
  */
 public class Solution {
 
