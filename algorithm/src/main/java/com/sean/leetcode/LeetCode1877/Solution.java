746e35bf3295229f84ed4f6e1eb005b49737cbd0
xionghaiyang <18756618469@163.com>
2026-01-24 11:24:26 +0800

    1877. 数组中最大数对和的最小值

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1877/Solution.java
@@ -0,0 +1,34 @@
+package com.sean.leetcode.LeetCode1877;
+
+import java.util.Arrays;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2026-01-24 11:15
+ * @Description https://leetcode.cn/problems/minimize-maximum-pair-sum-in-array
+ * 1877. 数组中最大数对和的最小值
+ * 一个数对 (a,b) 的 数对和 等于 a + b 。
+ * 最大数对和 是一个数对数组中最大的 数对和 。
+ * 比方说，如果我们有数对 (1,5) ，(2,3) 和 (4,4)，最大数对和 为 max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8 。
+ * 给你一个长度为 偶数 n 的数组 nums ，请你将 nums 中的元素分成 n / 2 个数对，使得：
+ * nums 中每个元素 恰好 在 一个 数对中，且
+ * 最大数对和 的值 最小 。
+ * 请你在最优数对划分的方案下，返回最小的 最大数对和 。
+ * n == nums.length
+ * 2 <= n <= 10^5
+ * n 是 偶数 。
+ * 1 <= nums[i] <= 105
+ */
+public class Solution {
+
+    public int minPairSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            res = Math.max(res, nums[i] + nums[j]);
+        }
+        return res;
+    }
+
+}
