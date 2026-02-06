a7d6feda090fc3b2be2c9d116822f59134f71582
xionghaiyang <18756618469@163.com>
2026-02-07 07:26:02 +0800

    1653. 使字符串平衡的最少删除次数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1653/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1653/Solution.java
@@ -3,35 +3,33 @@ package com.sean.leetcode.LeetCode1653;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-03-06 08:15
- * @Description: https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced/
+ * @Description: https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced
  * 1653. 使字符串平衡的最少删除次数
  * 给你一个字符串 s ，它仅包含字符 'a' 和 'b'​​​​ 。
  * 你可以删除 s 中任意数目的字符，使得 s 平衡 。
  * 当不存在下标对 (i,j) 满足 i < j ，且 s[i] = 'b' 的同时 s[j]= 'a' ，此时认为 s 是 平衡 的。
  * 请你返回使 s 平衡 的 最少 删除次数。
+ * 1 <= s.length <= 10^5
+ * s[i] 要么是 'a' 要么是 'b'​ 。​
  */
 public class Solution {
 
     public int minimumDeletions(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
         int n = s.length();
-        int leftb = 0, righta = 0;
+        int leftB = 0, rightA = 0;
         for (int i = 0; i < n; i++) {
             if (s.charAt(i) == 'a') {
-                righta++;
+                rightA++;
             }
         }
-        int res = righta;
+        int res = rightA;
         for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if (c == 'a') {
-                righta--;
+            if (s.charAt(i) == 'a') {
+                rightA--;
             } else {
-                leftb++;
+                leftB++;
             }
-            res = Math.min(res, leftb + righta);
+            res = Math.min(res, leftB + rightA);
         }
         return res;
     }
