9567a7a8fe4615010e752c747a0fb58afbe9581d
xionghaiyang <18756618469@163.com>
2026-02-21 08:20:36 +0800

    762. 二进制表示中质数个计算置位

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode762/Solution.java
@@ -0,0 +1,26 @@
+package com.sean.leetcode.LeetCode762;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2026-02-21 07:49
+ * @Description https://leetcode.cn/problems/prime-number-of-set-bits-in-binary-representation
+ * 762. 二进制表示中质数个计算置位
+ * 给你两个整数 left 和 right ，在闭区间 [left, right] 范围内，统计并返回 计算置位位数为质数 的整数个数。
+ * 计算置位位数 就是二进制表示中 1 的个数。
+ * 例如， 21 的二进制表示 10101 有 3 个计算置位。
+ * 1 <= left <= right <= 10^6
+ * 0 <= right - left <= 10^4
+ */
+public class Solution {
+
+    public int countPrimeSetBits(int left, int right) {
+        int res = 0;
+        for (int i = left; i <= right; i++) {
+            if (((1 << Integer.bitCount(i)) & 665772) != 0) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+}
