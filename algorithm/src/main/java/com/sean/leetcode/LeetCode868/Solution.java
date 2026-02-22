7d57da3ddb1ec97f26de5b1c08e5910df9f07d87
xionghaiyang <18756618469@163.com>
2026-02-22 08:17:15 +0800

    868. 二进制间距

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode868/Solution.java
@@ -0,0 +1,31 @@
+package com.sean.leetcode.LeetCode868;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2026-02-22 08:08
+ * @Description https://leetcode.cn/problems/binary-gap
+ * 868. 二进制间距
+ * 给定一个正整数 n，找到并返回 n 的二进制表示中两个 相邻 1 之间的 最长距离 。
+ * 如果不存在两个相邻的 1，返回 0 。
+ * 如果只有 0 将两个 1 分隔开（可能不存在 0 ），则认为这两个 1 彼此 相邻 。
+ * 两个 1 之间的距离是它们的二进制表示中位置的绝对差。
+ * 例如，"1001" 中的两个 1 的距离为 3 。
+ * 1 <= n <= 10^9
+ */
+public class Solution {
+
+    public int binaryGap(int n) {
+        int res = 0, last = -1;
+        for (int i = 0; n != 0; i++) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    res = Math.max(res, i - last);
+                }
+                last = i;
+            }
+            n >>= 1;
+        }
+        return res;
+    }
+
+}
