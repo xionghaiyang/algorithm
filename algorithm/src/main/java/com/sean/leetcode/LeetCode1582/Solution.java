4d947afd22aad2f5e12398fa9086e91ec0fb5643
xionghaiyang <18756618469@163.com>
2026-03-04 07:52:41 +0800

    1582. 二进制矩阵中的特殊位置

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1582/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1582/Solution.java
@@ -3,14 +3,18 @@ package com.sean.leetcode.LeetCode1582;
 /**
  * @Author xionghaiyang
  * @Date 2022-09-04 11:51
- * @Description https://leetcode.cn/problems/special-positions-in-a-binary-matrix/
+ * @Description https://leetcode.cn/problems/special-positions-in-a-binary-matrix
  * 1582. 二进制矩阵中的特殊位置
  * 给你一个大小为 rows x cols 的矩阵 mat，其中 mat[i][j] 是 0 或 1，请返回 矩阵 mat 中特殊位置的数目 。
  * 特殊位置 定义：如果 mat[i][j] == 1 并且第 i 行和第 j 列中的所有其他元素均为 0（行和列的下标均 从 0 开始 ），则位置 (i, j) 被称为特殊位置。
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 100
+ * mat[i][j] 是 0 或 1。
  */
 public class Solution {
 
-    public int numSpecial1(int[][] mat) {
+    public int numSpecial(int[][] mat) {
         int m = mat.length, n = mat[0].length;
         int[] rowsSum = new int[m];
         int[] colsSum = new int[n];
@@ -31,7 +35,7 @@ public class Solution {
         return res;
     }
 
-    public int numSpecial(int[][] mat) {
+    public int numSpecial1(int[][] mat) {
         int m = mat.length, n = mat[0].length;
         for (int i = 0; i < m; i++) {
             int cnt = 0;
