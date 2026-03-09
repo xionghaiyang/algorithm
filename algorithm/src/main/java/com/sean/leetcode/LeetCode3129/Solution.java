d5429641920550db783fc06a7c0f26640248792e
xionghaiyang <18756618469@163.com>
2026-03-09 09:11:14 +0800

    3129. 找出所有稳定的二进制数组 I

--- algorithm/src/main/java/com/sean/leetcode/LeetCode3129/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode3129/Solution.java
@@ -1,9 +1,11 @@
 package com.sean.leetcode.LeetCode3129;
 
+import java.util.Arrays;
+
 /**
  * @Author xionghaiyang
  * @Date 2024-08-06 17:29
- * @Description https://leetcode.cn/problems/find-all-possible-stable-binary-arrays-i/description/
+ * @Description https://leetcode.cn/problems/find-all-possible-stable-binary-arrays-i
  * 3129. 找出所有稳定的二进制数组 I
  * 给你 3 个正整数 zero ，one 和 limit 。
  * 一个二进制数组arr 如果满足以下条件，那么我们称它是 稳定的 ：
@@ -13,37 +15,46 @@ package com.sean.leetcode.LeetCode3129;
  * 子数组都 同时 包含 0 和 1 。
  * 请你返回 稳定 二进制数组的 总 数目。
  * 由于答案可能很大，将它对 10^9 + 7 取余 后返回。
+ * 1 <= zero, one, limit <= 200
  */
 public class Solution {
 
-    private static final long MOD = 1_000_000_007;
+    private static final int MOD = 1_000_000_007;
+    private int limit;
 
     public int numberOfStableArrays(int zero, int one, int limit) {
-        //dp[i][j][k]表示已经填入i个0和j个1，并且最后填入的数字为k(0或者1)的可行方案数目
-        long[][][] dp = new long[zero + 1][one + 1][2];
-        for (int i = 0; i <= Math.min(zero, limit); i++) {
-            dp[i][0][0] = 1;
-        }
-        for (int j = 0; j <= Math.min(one, limit); j++) {
-            dp[0][j][1] = 1;
-        }
-        for (int i = 1; i <= zero; i++) {
-            for (int j = 1; j <= one; j++) {
-                if (i > limit) {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1] - dp[i - limit - 1][j][1];
-                } else {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
-                }
-                dp[i][j][0] = (dp[i][j][0] % MOD + MOD) % MOD;
-                if (j > limit) {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0] - dp[i][j - limit - 1][0];
-                } else {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
-                }
-                dp[i][j][1] = (dp[i][j][1] % MOD + MOD) % MOD;
+        this.limit = limit;
+        int[][][] memo = new int[zero + 1][one + 1][2];
+        for (int i = 0; i <= zero; i++) {
+            for (int j = 0; j <= one; j++) {
+                Arrays.fill(memo[i][j], -1);
             }
         }
-        return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+        return (process(memo, zero, one, 0) + process(memo, zero, one, 1)) % MOD;
+    }
+
+    private int process(int[][][] memo, int i, int j, int k) {
+        if (memo[i][j][k] != -1) {
+            return memo[i][j][k];
+        }
+        if (i == 0) {
+            memo[i][j][k] = k == 1 && j <= limit ? 1 : 0;
+            return memo[i][j][k];
+        }
+        if (j == 0) {
+            memo[i][j][k] = k == 0 && i <= limit ? 1 : 0;
+            return memo[i][j][k];
+        }
+        if (k == 0) {
+            memo[i][j][k] = (int) (((long) process(memo, i - 1, j, 0)
+                    + process(memo, i - 1, j, 1)
+                    + (i > limit ? MOD - process(memo, i - limit - 1, j, 1) : 0)) % MOD);
+        } else {
+            memo[i][j][k] = (int) (((long) process(memo, i, j - 1, 0)
+                    + process(memo, i, j - 1, 1)
+                    + (j > limit ? MOD - process(memo, i, j - limit - 1, 0) : 0)) % MOD);
+        }
+        return memo[i][j][k];
     }
 
 }
