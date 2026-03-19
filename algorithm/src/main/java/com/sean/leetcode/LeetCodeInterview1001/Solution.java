1faf6823ab209b842e94285aaa0e07e03e869b0f
xionghaiyang <18756618469@163.com>
2026-03-19 12:35:08 +0800

    面试题 10.01. 合并排序的数组

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview1001/Solution.java
@@ -0,0 +1,32 @@
+package com.sean.leetcode.LeetCodeInterview1001;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2026-03-19 12:22
+ * @Description https://leetcode.cn/problems/sorted-merge-lcci
+ * 面试题 10.01. 合并排序的数组
+ * 给定两个排序后的数组 A 和 B，其中 A 的末端有足够的缓冲空间容纳 B。
+ * 编写一个方法，将 B 合并入 A 并排序。
+ * 初始化 A 和 B 的元素数量分别为 m 和 n。
+ * A.length == n + m
+ */
+public class Solution {
+
+    public void merge(int[] A, int m, int[] B, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 || j >= 0) {
+            if (i >= 0 && j >= 0) {
+                if (A[i] >= B[j]) {
+                    A[k--] = A[i--];
+                } else {
+                    A[k--] = B[j--];
+                }
+            } else if (i >= 0) {
+                A[k--] = A[i--];
+            } else {
+                A[k--] = B[j--];
+            }
+        }
+    }
+
+}
