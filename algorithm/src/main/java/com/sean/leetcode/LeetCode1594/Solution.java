a26c9601ee977d08c18c6d5610d56453cdf23b2b
xionghaiyang <18756618469@163.com>
2026-03-23 10:17:43 +0800

    1594. 矩阵的最大非负积

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1594/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1594/Solution.java
@@ -64,14 +64,28 @@ public class Solution {
 
     public int maxProductPath1(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        long[][][] memo = new long[m][n][2];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                Arrays.fill(memo[i][j], Long.MIN_VALUE);
+        long[][][] dp = new long[m][n][2];
+        dp[0][0][0] = grid[0][0];
+        dp[0][0][1] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0][0] = dp[i - 1][0][0] * grid[i][0];
+            dp[i][0][1] = dp[i - 1][0][1] * grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j][0] = dp[0][j - 1][0] * grid[0][j];
+            dp[0][j][1] = dp[0][j - 1][1] * grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                long a = dp[i - 1][j][0] * grid[i][j];
+                long b = dp[i - 1][j][1] * grid[i][j];
+                long c = dp[i][j - 1][0] * grid[i][j];
+                long d = dp[i][j - 1][1] * grid[i][j];
+                dp[i][j][0] = Math.min(Math.min(a, b), Math.min(c, d));
+                dp[i][j][1] = Math.max(Math.max(a, b), Math.max(c, d));
             }
         }
-        long res = process(grid, memo, m - 1, n - 1)[1];
-        return res < 0 ? -1 : (int) (res % MOD);
+        return dp[m - 1][n - 1][1] < 0 ? -1 : (int) (dp[m - 1][n - 1][1] % MOD);
     }
 
 }
