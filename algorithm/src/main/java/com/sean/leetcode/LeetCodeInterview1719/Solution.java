aad6b1c961d1b377dd9ee354c5eb7c0d04e51ed6
xionghaiyang <18756618469@163.com>
2026-03-25 22:32:05 +0800

    面试题 17.19. 消失的两个数字

--- algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview1719/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview1719/Solution.java
@@ -3,10 +3,12 @@ package com.sean.leetcode.LeetCodeInterview1719;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-09-26 08:20
- * @Description: https://leetcode.cn/problems/missing-two-lcci/
+ * @Description: https://leetcode.cn/problems/missing-two-lcci
  * 面试题 17.19. 消失的两个数字
- * 给定一个数组，包含从 1 到 N 所有的整数，但其中缺了两个数字。你能在 O(N) 时间内只用 O(1) 的空间找到它们吗？
+ * 给定一个数组，包含从 1 到 N 所有的整数，但其中缺了两个数字。
+ * 你能在 O(N) 时间内只用 O(1) 的空间找到它们吗？
  * 以任意顺序返回这两个数字均可。
+ * nums.length <= 30000
  */
 public class Solution {
 
@@ -19,23 +21,19 @@ public class Solution {
         for (int i = 1; i <= n; i++) {
             xor ^= i;
         }
-        xor = xor & (-xor);
-        int res1 = 0, res2 = 0;
+        int xor1 = xor ^ (-xor);
+        int res = 0;
         for (int num : nums) {
-            if ((num & xor) != 0) {
-                res1 ^= num;
-            } else {
-                res2 ^= num;
+            if ((num & xor1) != 0) {
+                res ^= num;
             }
         }
         for (int i = 1; i <= n; i++) {
-            if ((i & xor) != 0) {
-                res1 ^= i;
-            } else {
-                res2 ^= i;
+            if ((i & xor1) != 0) {
+                res ^= i;
             }
         }
-        return new int[]{res1, res2};
+        return new int[]{res, res ^ xor};
     }
 
 }
