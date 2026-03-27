9136e8fbd0ea21c9f02efd3f657bf39c6bac7926
xionghaiyang <18756618469@163.com>
2026-03-27 13:34:16 +0800

    面试题 17.24. 最大子矩阵

--- algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview1724/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCodeInterview1724/Solution.java
@@ -1,9 +1,11 @@
 package com.sean.leetcode.LeetCodeInterview1724;
 
+import java.util.Arrays;
+
 /**
  * @Auther: xionghaiyang
  * @Date: 2024-01-19 13:27
- * @Description: https://leetcode.cn/problems/max-submatrix-lcci/
+ * @Description: https://leetcode.cn/problems/max-submatrix-lcci
  * 面试题 17.24. 最大子矩阵
  * 给定一个正整数、负整数和 0 组成的 N × M 矩阵，编写代码找出元素总和最大的子矩阵。
  * 返回一个数组 [r1, c1, r2, c2]，其中 r1, c1 分别代表子矩阵左上角的行号和列号，r2, c2 分别代表右下角的行号和列号。
@@ -13,40 +15,29 @@ package com.sean.leetcode.LeetCodeInterview1724;
 public class Solution {
 
     public int[] getMaxMatrix(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int[][] preSum = new int[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                preSum[i][j] = matrix[i - 1][j - 1] + preSum[i - 1][j] + preSum[i][j - 1] - preSum[i - 1][j - 1];
-            }
-        }
-        int s = Integer.MIN_VALUE;
-        int[] res = new int[]{0, 0, 0, 0};
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int[] num = new int[m];
-                for (int k = 0; k < m; k++) {
-                    num[k] = preSum[j + 1][k + 1] + preSum[i][k] - preSum[j + 1][k] - preSum[i][k + 1];
-                }
-                int start = 0;
-                int sum = num[0];
-                if (sum > s) {
-                    res = new int[]{i, start, j, 0};
-                    s = sum;
-                }
-                for (int k = 1; k < m; k++) {
+        int[] res = new int[4];
+        int m = matrix.length, n = matrix[0].length;
+        int[] nums = new int[n];
+        int r1 = 0, c1 = 0, sum = 0, maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(nums, 0);
+            for (int j = i; j < m; j++) {
+                sum = 0;
+                for (int k = 0; k < n; k++) {
+                    nums[k] += matrix[j][k];
                     if (sum > 0) {
-                        sum += num[k];
+                        sum += nums[k];
                     } else {
-                        sum = num[k];
-                        start = k;
+                        sum = nums[k];
+                        r1 = i;
+                        c1 = k;
                     }
-                    if (sum > s) {
-                        res = new int[]{i, start, j, k};
-                        s = sum;
+                    if (sum > maxSum) {
+                        maxSum = sum;
+                        res = new int[]{r1, c1, j, k};
                     }
                 }
+
             }
         }
         return res;
