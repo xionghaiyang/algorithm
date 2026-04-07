8ec155f9c354b58f651243c468278b039bb9effe
xionghaiyang <18756618469@163.com>
2026-04-08 07:16:05 +0800

    3653. 区间乘法查询后的异或 I

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode3653/Solution.java
@@ -0,0 +1,41 @@
+package com.sean.leetcode.LeetCode3653;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2026-04-08 06:56
+ * @Description https://leetcode.cn/problems/xor-after-range-multiplication-queries-i
+ * 3653. 区间乘法查询后的异或 I
+ * 给你一个长度为 n 的整数数组 nums 和一个大小为 q 的二维整数数组 queries，其中 queries[i] = [li, ri, ki, vi]。
+ * 对于每个查询，按以下步骤执行操作：
+ * 设定 idx = li。
+ * 当 idx <= ri 时：
+ * 更新：nums[idx] = (nums[idx] * vi) % (10^9 + 7)
+ * 将 idx += ki。
+ * 在处理完所有查询后，返回数组 nums 中所有元素的 按位异或 结果。
+ * 1 <= n == nums.length <= 10^3
+ * 1 <= nums[i] <= 10^9
+ * 1 <= q == queries.length <= 10^3
+ * queries[i] = [li, ri, ki, vi]
+ * 0 <= li <= ri < n
+ * 1 <= ki <= n
+ * 1 <= vi <= 10^5
+ */
+public class Solution {
+
+    private static final int MOD = 1_000_000_007;
+
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        for (int[] query : queries) {
+            int l = query[0], r = query[1], k = query[2], v = query[3];
+            for (int i = l; i <= r; i += k) {
+                nums[i] = (int) (((long) nums[i] * v) % MOD);
+            }
+        }
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+
+}
