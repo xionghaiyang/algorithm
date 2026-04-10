8baf043c3945d42080b1aaaca48ece53be35d34f
xionghaiyang <18756618469@163.com>
2026-04-10 12:46:46 +0800

    860. 柠檬水找零

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode860/Solution.java
@@ -0,0 +1,41 @@
+package com.sean.leetcode.LeetCode860;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2026-04-10 12:28
+ * @Description https://leetcode.cn/problems/lemonade-change
+ * 860. 柠檬水找零
+ * 在柠檬水摊上，每一杯柠檬水的售价为 5 美元。
+ * 顾客排队购买你的产品，（按账单 bills 支付的顺序）一次购买一杯。
+ * 每位顾客只买一杯柠檬水，然后向你付 5 美元、10 美元或 20 美元。
+ * 你必须给每个顾客正确找零，也就是说净交易是每位顾客向你支付 5 美元。
+ * 注意，一开始你手头没有任何零钱。
+ * 给你一个整数数组 bills ，其中 bills[i] 是第 i 位顾客付的账。
+ * 如果你能给每位顾客正确找零，返回 true ，否则返回 false 。
+ * 1 <= bills.length <= 10^5
+ * bills[i] 不是 5 就是 10 或是 20
+ */
+public class Solution {
+
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                ten++;
+                five--;
+            } else if (ten > 0) {
+                ten--;
+                five--;
+            } else {
+                five -= 3;
+            }
+            if (five < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
