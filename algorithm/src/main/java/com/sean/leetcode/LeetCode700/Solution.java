658336e18907af463ac9ec79ff3f409af5ba9ae9
xionghaiyang <18756618469@163.com>
2026-04-11 20:26:25 +0800

    700. 二叉搜索树中的搜索

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode700/Solution.java
@@ -0,0 +1,51 @@
+package com.sean.leetcode.LeetCode700;
+
+/**
+ * @Author xionghaiyang
+ * @Date 2026-04-11 20:00
+ * @Description https://leetcode.cn/problems/search-in-a-binary-search-tree
+ * 700. 二叉搜索树中的搜索
+ * 给定二叉搜索树（BST）的根节点 root 和一个整数值 val。
+ * 你需要在 BST 中找到节点值等于 val 的节点。
+ * 返回以该节点为根的子树。
+ * 如果节点不存在，则返回 null 。
+ * 树中节点数在 [1, 5000] 范围内
+ * 1 <= Node.val <= 10^7
+ * root 是二叉搜索树
+ * 1 <= val <= 10^7
+ */
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+
+}
