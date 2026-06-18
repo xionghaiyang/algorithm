a052d037840a32f86963417e50640b873ad96288
xionghaiyang <18756618469@163.com>
2026-06-18 18:11:44 +0800

    2095. 删除链表的中间节点

--- algorithm/src/main/java/com/sean/leetcode/LeetCode2095/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode2095/Solution.java
@@ -3,11 +3,14 @@ package com.sean.leetcode.LeetCode2095;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-06-15 09:43
- * @Description: https://leetcode.cn/problems/delete-the-middle-node-of-a-linked-list/?envType=study-plan-v2&envId=leetcode-75
+ * @Description: https://leetcode.cn/problems/delete-the-middle-node-of-a-linked-list
  * 2095. 删除链表的中间节点
- * 给你一个链表的头节点 head 。删除 链表的 中间节点 ，并返回修改后的链表的头节点 head 。
+ * 给你一个链表的头节点 head 。
+ * 删除 链表的 中间节点 ，并返回修改后的链表的头节点 head 。
  * 长度为 n 链表的中间节点是从头数起第 ⌊n / 2⌋ 个节点（下标从 0 开始），其中 ⌊x⌋ 表示小于或等于 x 的最大整数。
  * 对于 n = 1、2、3、4 和 5 的情况，中间节点的下标分别是 0、1、1、2 和 2 。
+ * 链表中节点的数目在范围 [1, 10^5] 内
+ * 1 <= Node.val <= 10^5
  */
 public class Solution {
 
@@ -29,18 +32,16 @@ public class Solution {
     }
 
     public ListNode deleteMiddle(ListNode head) {
-        if (head == null || head.next == null) {
+        if (head.next == null) {
             return null;
         }
         ListNode slow = head;
-        ListNode fast = head;
-        ListNode pre = null;
+        ListNode fast = head.next.next;
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            pre = slow;
             slow = slow.next;
+            fast = fast.next.next;
         }
-        pre.next = pre.next.next;
+        slow.next = slow.next.next;
         return head;
     }
 
