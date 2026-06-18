d008db47b3b1160bdbbd6e4c103ee4e257c36e15
xionghaiyang <18756618469@163.com>
2026-06-18 13:59:13 +0800

    2130. 链表最大孪生和

--- algorithm/src/main/java/com/sean/leetcode/LeetCode2130/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode2130/Solution.java
@@ -3,7 +3,7 @@ package com.sean.leetcode.LeetCode2130;
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-06-15 10:26
- * @Description: https://leetcode.cn/problems/maximum-twin-sum-of-a-linked-list/?envType=study-plan-v2&envId=leetcode-75
+ * @Description: https://leetcode.cn/problems/maximum-twin-sum-of-a-linked-list
  * 2130. 链表最大孪生和
  * 在一个大小为 n 且 n 为 偶数 的链表中，对于 0 <= i <= (n / 2) - 1 的 i ，
  * 第 i 个节点（下标从 0 开始）的孪生节点为第 (n-1-i) 个节点 。
@@ -11,6 +11,8 @@ package com.sean.leetcode.LeetCode2130;
  * 这是长度为 n = 4 的链表中所有的孪生节点。
  * 孪生和 定义为一个节点和它孪生节点两者值之和。
  * 给你一个长度为偶数的链表的头节点 head ，请你返回链表的 最大孪生和 。
+ * 链表的节点数目是 [2, 10^5] 中的 偶数 。
+ * 1 <= Node.val <= 10^5
  */
 public class Solution {
 
@@ -46,8 +48,7 @@ public class Solution {
             slow.next = cur;
         }
         int res = 0;
-        ListNode x = head;
-        ListNode y = slow.next;
+        ListNode x = head, y = slow.next;
         while (y != null) {
             res = Math.max(res, x.val + y.val);
             x = x.next;
