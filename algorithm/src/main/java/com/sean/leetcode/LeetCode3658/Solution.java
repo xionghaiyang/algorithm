90104f11f171c1ebb9d1a2b21ec98b5b2413cd33
xionghaiyang <18756618469@163.com>
2026-07-15 07:56:54 +0800

    3658. 奇数和与偶数和的最大公约数

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode3658/Solution.java
@@ -0,0 +1,20 @@
+package com.sean.leetcode.LeetCode3658;
+
+/**
+ * @Author: xionghaiyang
+ * @Date: 2026-07-15 07:55
+ * @Description: https://leetcode.cn/problems/gcd-of-odd-and-even-sums
+ * 3658. 奇数和与偶数和的最大公约数
+ * 给你一个整数 n。请你计算以下两个值的 最大公约数（GCD）：
+ * sumOdd：最小的 n 个正奇数的总和。
+ * sumEven：最小的 n 个正偶数的总和。
+ * 返回 sumOdd 和 sumEven 的 GCD。
+ * 1 <= n <= 1000
+ */
+public class Solution {
+
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+
+}
