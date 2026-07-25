079e06e07c9cae63b2dc538f656e15d9c3a968f1
xionghaiyang <18756618469@163.com>
2026-07-26 07:07:16 +0800

    628. 三个数的最大乘积

--- algorithm/src/main/java/com/sean/leetcode/LeetCode628/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode628/Solution.java
@@ -1,20 +1,38 @@
 package com.sean.leetcode.LeetCode628;
 
-import java.util.Arrays;
-
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-12-24 19:34
- * @Description: https://leetcode.cn/problems/maximum-product-of-three-numbers/description/
+ * @Description: https://leetcode.cn/problems/maximum-product-of-three-numbers
  * 628. 三个数的最大乘积
  * 给你一个整型数组 nums ，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ * 3 <= nums.length <= 10^4
+ * -1000 <= nums[i] <= 1000
  */
 public class Solution {
 
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1]);
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+        }
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
     }
 
 }
