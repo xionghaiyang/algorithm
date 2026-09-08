09db903657c5cab3172f374f6183003a4ac14d89
xionghaiyang <18756618469@163.com>
2026-09-08 13:33:08 +0800

    621. 任务调度器

---
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode621/Solution.java
@@ -0,0 +1,41 @@
+package com.sean.leetcode.LeetCode621;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: xionghaiyang
+ * @Date: 2026-09-08 13:01
+ * @Description: https://leetcode.cn/problems/task-scheduler
+ * 621. 任务调度器
+ * 给你一个用字符数组 tasks 表示的 CPU 需要执行的任务列表，用字母 A 到 Z 表示，以及一个冷却时间 n。
+ * 每个周期或时间间隔允许完成一项任务。
+ * 任务可以按任何顺序完成，但有一个限制：两个 相同种类 的任务之间必须有长度为 n 的冷却时间。
+ * 返回完成所有任务所需要的 最短时间间隔 。
+ * 1 <= tasks.length <= 10^4
+ * tasks[i] 是大写英文字母
+ * 0 <= n <= 100
+ */
+public class Solution {
+
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> map = new HashMap<>();
+        //最多的执行次数
+        int maxExec = 0;
+        for (char task : tasks) {
+            int exec = map.getOrDefault(task, 0) + 1;
+            map.put(task, exec);
+            maxExec = Math.max(maxExec, exec);
+        }
+        //具有最多执行任务次数的任务数量
+        int maxCount = 0;
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            int value = entry.getValue();
+            if (value == maxExec) {
+                maxCount++;
+            }
+        }
+        return Math.max((maxExec - 1) * (n + 1) + maxCount, tasks.length);
+    }
+
+}
