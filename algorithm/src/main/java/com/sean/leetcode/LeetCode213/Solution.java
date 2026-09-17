a14d8ffca5c56333e59a779f39d3dfc643fc5c27
xionghaiyang <18756618469@163.com>
2026-09-17 19:48:53 +0800

    213. 打家劫舍 II

--- algorithm/src/main/java/com/sean/leetcode/LeetCode213/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode213/Solution.java
@@ -3,18 +3,18 @@ package com.sean.leetcode.LeetCode213;
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-14 20:03
- * @Description: https://leetcode.cn/problems/house-robber-ii/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/house-robber-ii
  * 213. 打家劫舍 II
  * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。
- * 这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+ * 这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。
+ * 同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
  * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
  */
 public class Solution {
 
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         int n = nums.length;
         if (n == 1) {
             return nums[0];
