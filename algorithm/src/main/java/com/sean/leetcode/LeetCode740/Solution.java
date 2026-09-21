8ea4b525ebedfba04636c9e41ca6fb8fa87de3fd
xionghaiyang <18756618469@163.com>
2026-09-21 18:34:02 +0800

    740. 删除并获得点数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode740/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode740/Solution.java
@@ -1,40 +1,40 @@
 package com.sean.leetcode.LeetCode740;
 
-import java.util.Arrays;
-
 /**
  * @Auther: xionghaiyang
  * @Date: 2022-12-14 14:11
- * @Description: https://leetcode.cn/problems/delete-and-earn/?envType=study-plan&id=dong-tai-gui-hua-ru-men&plan=dynamic-programming&plan_progress=yn1qp15
+ * @Description: https://leetcode.cn/problems/delete-and-earn
  * 740. 删除并获得点数
  * 给你一个整数数组 nums ，你可以对它进行一些操作。
  * 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数。
  * 之后，你必须删除 所有 等于 nums[i] - 1 和 nums[i] + 1 的元素。
- * 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
+ * 开始你拥有 0 个点数。
+ * 返回你能通过这些操作获得的最大点数。
+ * 1 <= nums.length <= 2 * 10^4
+ * 1 <= nums[i] <= 10^4
  */
 public class Solution {
 
     public int deleteAndEarn(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int max = Arrays.stream(nums).max().getAsInt();
-        int[] sum = new int[max + 1];
+        int max = 0;
         for (int num : nums) {
-            sum[num] += num;
+            max = Math.max(max, num);
         }
-        return rob(sum);
+        int[] arr = new int[max + 1];
+        for (int num : nums) {
+            arr[num] += num;
+        }
+        return rob(arr);
     }
 
     private int rob(int[] nums) {
-        int size = nums.length;
-        int first = nums[0], second = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < size; i++) {
-            int temp = second;
-            second = Math.max(first + nums[i], second);
-            first = temp;
+        int f0 = 0, f1 = 0;
+        for (int num : nums) {
+            int newF = Math.max(f1, f0 + num);
+            f0 = f1;
+            f1 = newF;
         }
-        return second;
+        return f1;
     }
 
 }
