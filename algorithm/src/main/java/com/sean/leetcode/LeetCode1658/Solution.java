848ccbb63c7ddf3a952f09e877e17031ff00caa9
xionghaiyang <18756618469@163.com>
2026-09-23 06:22:20 +0800

    1658. 将 x 减到 0 的最小操作数

--- algorithm/src/main/java/com/sean/leetcode/LeetCode1658/Solution.java
+++ algorithm/src/main/java/com/sean/leetcode/LeetCode1658/Solution.java
@@ -1,40 +1,38 @@
 package com.sean.leetcode.LeetCode1658;
 
-import java.util.Arrays;
-
 /**
  * @Auther: xionghaiyang
  * @Date: 2023-01-09 09:44
- * @Description: https://leetcode.cn/problems/minimum-operations-to-reduce-x-to-zero/
+ * @Description: https://leetcode.cn/problems/minimum-operations-to-reduce-x-to-zero
  * 1658. 将 x 减到 0 的最小操作数
  * 给你一个整数数组 nums 和一个整数 x 。
  * 每一次操作时，你应当移除数组 nums 最左边或最右边的元素，然后从 x 中减去该元素的值。
  * 请注意，需要 修改 数组以供接下来的操作使用。
  * 如果可以将 x 恰好 减到 0 ，返回 最小操作数 ；否则，返回 -1 。
+ * 1 <= nums.length <= 10^5
+ * 1 <= nums[i] <= 10^4
+ * 1 <= x <= 10^9
  */
 public class Solution {
 
     public int minOperations(int[] nums, int x) {
-        if (nums == null || nums.length == 0) {
-            return x == 0 ? 0 : -1;
-        }
         int n = nums.length;
-        int sum = Arrays.stream(nums).sum();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
         if (sum < x) {
             return -1;
         }
-        int right = 0;
-        int lsum = 0, rsum = sum;
         int res = n + 1;
-        for (int left = -1; left < n; left++) {
-            if (left != -1) {
-                lsum += nums[left];
+        for (int left = -1, right = 0, leftSum = 0, rightSum = sum; left < n; left++) {
+            if (left >= 0) {
+                leftSum += nums[left];
             }
-            while (right < n && lsum + rsum > x) {
-                rsum -= nums[right];
-                right++;
+            while (right < n && leftSum + rightSum > x) {
+                rightSum -= nums[right++];
             }
-            if (lsum + rsum == x) {
+            if (leftSum + rightSum == x) {
                 res = Math.min(res, (left + 1) + (n - right));
             }
         }
